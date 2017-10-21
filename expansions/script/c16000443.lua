75b9d708b3a0a7022844cf02a01c8b5c23c2ae0a
YGOCC <chadoook1990@gmail.com>
2017-10-21 23:42:10 +0100

    Update

--- expansions/script/c16000443.lua
+++ expansions/script/c16000443.lua
@@ -1,6 +1,78 @@
 --Paintress-Ama Archfiend Goghi
 function c16000443.initial_effect(c)
    --link summon
-	aux.AddLinkProcedure(c,nil,2,2)
-	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,c160001231.mfilter,2)
+	c:EnableReviveLimit()  
+  --special summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetDescription(aux.Stringid(6666,6))
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCondition(c16000443.spcon)
+	e0:SetOperation(c16000443.spop)
+	e0:SetValue(SUMMON_TYPE_LINK)
+	c:RegisterEffect(e0)
+   --cannot be target/battle indestructable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(c16000443.tgtg)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+--atkup
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(16000443,0))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCondition(c16000443.tdcon)
+	e3:SetTarget(c16000443.tdtg)
+	e3:SetOperation(c16000443.tdop)
+	c:RegisterEffect(e3)
+end
+function c16000443.mfilter(c)
+	return  not c:IsLinkType(TYPE_EFFECT)
+end
+function c16000443.rfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xc50) and c:IsAbleToRemoveAsCost()
+end
+function c16000443.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c16000443.rfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,3,nil)
+end
+function c16000443.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c16000443.rfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,3,3,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_LINK)
+end
+function c16000443.tgtg(e,c)
+	return e:GetHandler():GetLinkedGroup():IsContains(c)
+end
+function c16000443.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function c16000443.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,PLAYER_ALL,LOCATION_REMOVED)
+end
+function c16000443.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetFieldGroup(tp,LOCATION_REMOVED,0)
+	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK)
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+	Duel.Recover(tp,ct*500,REASON_EFFECT)
+	
+	end
 end
---
+++ expansions/script/c16000506.lua
@@ -0,0 +1,92 @@
+--Daddou, Final Mage of Magnificent VINE 
+function c16000506.initial_effect(c)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x85a),3)
+	c:EnableReviveLimit()
+
+   --act limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetOperation(c16000506.chainop)
+	c:RegisterEffect(e1)
+  --special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(16000506,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCondition(c16000506.spcon)
+	e2:SetTarget(c16000506.target2)
+	e2:SetOperation(c16000506.activate2)
+	c:RegisterEffect(e2)
+
+local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(16000506,1))
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1)
+	e5:SetCost(c16000506.matcost)
+	e5:SetTarget(c16000506.mattg)
+	e5:SetOperation(c16000506.matop)
+	c:RegisterEffect(e5)
+end
+
+function c16000506.chainop(e,tp,eg,ep,ev,re,r,rp)
+	local rc=re:GetHandler()
+	if  rc:IsSetCard(0x285a)  then
+		Duel.SetChainLimit(c16000506.chainlm)
+	end
+end
+function c16000506.chainlm(e,rp,tp)
+	return tp==rp
+end
+function c16000506.filter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x85a) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToHand()
+end
+function c16000506.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function c16000506.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:GetControler()==tp and chkc:GetLocation()==LOCATION_GRAVE and c16000506.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c16000506.filter,tp,LOCATION_GRAVE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c16000506.filter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,2,0,0)
+end
+function c16000506.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if sg:GetCount()>0 then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	end
+end
+function c16000506.tgfilter(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x285a)
+end
+function c16000506.matcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function c16000506.matfilter(c)
+	return c:IsType(TYPE_MONSTER) and not c:IsType(TYPE_TOKEN)
+end
+function c16000506.mattg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c16000506.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c16000506.tgfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(c16000506.matfilter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c16000506.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c16000506.matop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local g=Duel.SelectMatchingCard(tp,c16000506.matfilter,tp,0,LOCATION_MZONE,1,1,nil)
+		if g:GetCount()>0 then
+			Duel.Overlay(tc,g)
+		end
+	end
+end
