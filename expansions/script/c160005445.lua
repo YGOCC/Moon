a9c8143f063def1bb63892e991decd267523b5bf
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2017-11-09 23:05:34 +0200

    Added a script for Dog Token and updated Da Vinca

--- expansions/script/c160005445.lua
+++ expansions/script/c160005445.lua
@@ -19,12 +19,9 @@ function c160005445.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_IGNITION)
 	e3:SetRange(LOCATION_PZONE)
 	e3:SetCountLimit(1,160005445)
-	e3:SetCondition(c160005445.pcon)
 	e3:SetTarget(c160005445.ptg)
 	e3:SetOperation(c160005445.pop)
 	c:RegisterEffect(e3)
-
-
 end
 function c160005445.splimit(e,c,sump,sumtype,sumpos,targetp)
 	if c:IsSetCard(0xc50) or c:IsType(TYPE_NORMAL) then return false end
@@ -37,13 +34,13 @@ ffunction c160005445.pfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_PENDULUM) and (c:IsSetCard(0xc50) or c:IsType(TYPE_NORMAL)) and c:IsAbleToHand()
 end
 function c160005445.ptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c160005445.pfilter,tp,LOCATION_EXTRA,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c160005445.pfilter,tp,LOCATION_EXTRA,0,2,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_EXTRA)
 end
 function c160005445.pop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c160005445.pfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c160005445.pfilter,tp,LOCATION_EXTRA,0,2,2,nil)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
---
+++ expansions/script/c50031664.lua
@@ -0,0 +1,4 @@
+--Mutated Dog Token
+function c17.initial_effect(c)
+
+end
