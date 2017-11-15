d5810c5cd5c20d7fd2ca592d97bc14656c20c58a
Clara Grace Paulsen <claragracepaulsen@gmail.com>
2017-11-15 03:01:45 -0500

    Update Lyris' cards
    Fix: Added "Accel Burst Dragon" to cDB
    Nerfed "Intercept Signal"
    * If activated, its user cannot Special Summon monsters
    for the next 2 turns.
    PSCT tweak for "Alexiand the Swordsmaster"

--- expansions/script/c240100003.lua
+++ expansions/script/c240100003.lua
@@ -1,6 +1,6 @@
 --Intercept Signal
 function c240100003.initial_effect(c)
-	--Activate as Chain Link 3 or higher when a card or effect is activated that includes an effect that Special Summons a monster(s): Negate the activations of your opponent's cards/effects activated before this card in this Chain, and if you do, place them on the bottom of the Deck in any order.
+	--Activate as Chain Link 3 or higher when a card or effect is activated that includes an effect that Special Summons a monster(s): You cannot Special Summon monsters for the rest of this turn and the next, also, negate the activations of your opponent's cards/effects activated before this card in this Chain, and if you do, place them on the bottom of the Deck.
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_TODECK)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
@@ -33,6 +33,13 @@ function c240100003.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,dg,dg:GetCount(),0,0)
 end
 function c240100003.activate(e,tp,eg,ep,ev,re,r,rp)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetReset(RESET_PHASE+PHASE_END,2)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,0)
+	Duel.RegisterEffect(e2,tp)
 	local dg=Group.CreateGroup()
 	for i=1,ev do
 		local te,tgp=Duel.GetChainInfo(i,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
