065d014b9c69f00cd09438ee27cc4e120085436e
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2018-01-07 00:21:30 +0200

    Updated Somen00b's cards

--- expansions/script/c249000799.lua
+++ expansions/script/c249000799.lua
@@ -8,6 +8,18 @@ function c249000799.initial_effect(c)
 	e1:SetTarget(c249000799.target)
 	e1:SetOperation(c249000799.activate)
 	c:RegisterEffect(e1)
+	--draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DRAW)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCondition(c249000799.drcon)
+	e2:SetCost(c249000799.drcost)
+	e2:SetTarget(c249000799.drtg)
+	e2:SetOperation(c249000799.drop)
+	c:RegisterEffect(e2)
 end
 function c249000799.filter(c)
 	return c:IsSetCard(0x1D8) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
@@ -23,4 +35,24 @@ function c249000799.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
+end
+function c249000799.cfilter(c,tp)
+	return c:IsFaceup() and c:GetSummonPlayer()==tp and c:GetReasonEffect():GetHandler():IsSetCard(0x1D8) and (c:IsType(TYPE_YXZ) or c:IsType(TYPE_SYNCHRO))
+end
+function c249000799.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c249000799.cfilter,1,nil,tp)
+end
+function c249000799.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c249000799.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function c249000799.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
