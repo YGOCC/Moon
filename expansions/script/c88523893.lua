489e16151e885720e2651c387d79e3e29adceb43
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2018-01-19 00:07:41 +0100

    Updated "Kitseki" Archetype
    Fixed "Kitseki Expectance" effect

--- expansions/script/c88523893.lua
+++ expansions/script/c88523893.lua
@@ -25,24 +25,34 @@ end
 function c88523893.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		--spell/trap negation
-		if re:GetHandler():IsType(TYPE_SPELL+TYPE_TRAP) and re:GetHandler():IsCanTurnSet() then
-			re:GetHandler():CancelToGrave()
-			Duel.ChangePosition(re:GetHandler(),POS_FACEDOWN)
-			re:GetHandler():SetStatus(STATUS_SET_TURN,false)
-			Duel.RaiseEvent(re:GetHandler(),EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
+		if re:GetHandler():IsType(TYPE_SPELL+TYPE_TRAP) then
+			local rc=re:GetHandler()
+			local seq=rc:GetSequence()
+			rc:CancelToGrave()
+			Duel.SendtoDeck(rc,nil,2,REASON_EFFECT)
+			local op=Duel.GetOperatedGroup()
+			local tc=op:GetFirst()
+			Duel.SSet(1-tp,tc)
+			Duel.MoveSequence(tc,seq)
+			Duel.ConfirmCards(1-tp,tc)
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_CANNOT_TRIGGER)
-			e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY)
-			re:GetHandler():RegisterEffect(e1)
+			e1:SetReset(RESET_EVENT+0x17a0000+RESET_PHASE+PHASE_STANDBY)
+			tc:RegisterEffect(e1)
 		--monster negation
 		elseif re:GetHandler():IsType(TYPE_MONSTER) and re:GetHandler():IsCanTurnSet() then
 			if Duel.ChangePosition(re:GetHandler(),POS_FACEDOWN_DEFENSE)~=0 then
-				local e1=Effect.CreateEffect(e:GetHandler())
-				e1:SetType(EFFECT_TYPE_SINGLE)
-				e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-				e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY)
-				re:GetHandler():RegisterEffect(e1)
+				local op=Duel.GetOperatedGroup()
+				local tc=op:GetFirst()
+				while tc do
+					local e1=Effect.CreateEffect(e:GetHandler())
+					e1:SetType(EFFECT_TYPE_SINGLE)
+					e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+					e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_STANDBY)
+					tc:RegisterEffect(e1)
+					tc=op:GetNext()
+				end
 			end
 		end
 	end
