4b3b5a6b08d53443c955a2da998be19bf0613223
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2018-01-18 20:56:06 +0200

    Updated Somen00b's cards, added ESPGear to both Moon and Terra, Added a Woondland fix

--- expansions/script/c249000789.lua
+++ expansions/script/c249000789.lua
@@ -20,12 +20,11 @@ function c249000789.initial_effect(c)
 	e3:SetCondition(c249000789.atkcon)
 	e3:SetValue(c249000789.atkup)
 	c:RegisterEffect(e3)
-	--copy trap
+	--set trap
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_QUICK_O)
-	e4:SetRange(LOCATION_GRAVE)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetHintTiming(0,0x1e1)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetCountLimit(1)
+	e4:SetRange(LOCATION_MZONE)
 	e4:SetCondition(c249000789.condition)
 	e4:SetCost(c249000789.cost)
 	e4:SetTarget(c249000789.target)
@@ -47,47 +46,38 @@ function c249000789.atkup(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_GRAVE,0,nil,RACE_SPELLCASTER)*300
 end
 function c249000789.condition(e,tp,eg,ep,ev,re,r,rp)
-	local ph=Duel.GetCurrentPhase()
-	if not (ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE) then return false end
 	local g=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,e:GetHandler(),0x30CF)
 	local ct=g:GetClassCount(Card.GetCode)
 	return ct>1
-end
-function c249000789.filter(c)
-	return c:GetType()==0x4 and c:IsAbleToRemoveAsCost() and c:CheckActivateEffect(false,true,false)~=nil
 end
 function c249000789.disfilter(c)
 	return c:IsRace(RACE_SPELLCASTER) and c:IsDiscardable()
 end
 function c249000789.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsAbleToRemoveAsCost()
-		and Duel.IsExistingMatchingCard(c249000789.filter,tp,LOCATION_GRAVE,0,1,nil)
-		and Duel.IsExistingMatchingCard(c249000789.disfilter,tp,LOCATION_HAND,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(249000789,1))
-	local g=Duel.SelectMatchingCard(tp,c249000789.filter,tp,LOCATION_GRAVE,0,1,1,nil)
-	local te=g:GetFirst():CheckActivateEffect(false,true,true)
-	c249000789[Duel.GetCurrentChain()]=te
-	g:AddCard(c)
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	if chk==0 then return Duel.IsExistingMatchingCard(c249000789.disfilter,tp,LOCATION_HAND,0,1,nil) end
 	Duel.DiscardHand(tp,c249000789.disfilter,1,1,REASON_COST+REASON_DISCARD)
 end
+function c249000789.setfilter(c)
+	return c:GetType()==0x4 and c:IsSSetable() and not c:IsHasEffect(EFFECT_NECRO_VALLEY)
+end
 function c249000789.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local te=c249000789[Duel.GetCurrentChain()]
-	if chkc then
-		local tg=te:GetTarget()
-		return tg(e,tp,eg,ep,ev,re,r,rp,0,true)
-	end
+	if chkc then return chkc:GetLocation()==LOCATION_GRAVE and chkc:GetControler()==tp and c249000789.setfilter(chkc) end
 	if chk==0 then return true end
-	if not te then return end
-	e:SetCategory(te:GetCategory())
-	e:SetProperty(te:GetProperty())
-	local tg=te:GetTarget()
-	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+	local g=Duel.SelectTarget(tp,c249000789.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 end
 function c249000789.operation(e,tp,eg,ep,ev,re,r,rp)
-	local te=c249000789[Duel.GetCurrentChain()]
-	if not te then return end
-	local op=te:GetOperation()
-	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsSSetable() then
+		Duel.SSet(tp,tc)
+		Duel.ConfirmCards(1-tp,tc)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x47e0000)
+		e1:SetValue(LOCATION_REMOVED)
+		tc:RegisterEffect(e1)
+	end
 end
