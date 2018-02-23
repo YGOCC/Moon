be999c989ae99400deea8a76d89bab2fc45c555d
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2018-02-23 23:24:48 +0200

    Updated RP's cards and Cyberse Jumper

--- expansions/script/c11528683.lua
+++ expansions/script/c11528683.lua
@@ -14,6 +14,7 @@ function c11528683.initial_effect(c)
 	e2:SetDescription(aux.Stringid(11528683,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetTarget(c11528683.thtg)
 	e2:SetOperation(c11528683.thop)
@@ -31,7 +32,7 @@ function c11528683.thfilter(c)
 	return c:IsSetCard(0x850) and not c:IsCode(11528683) and c:IsAbleToHand()
 end
 function c11528683.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingMatchingCard(c11528683.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function c11528683.thop(e,tp,eg,ep,ev,re,r,rp)
--- expansions/script/c11528685.lua
+++ expansions/script/c11528685.lua
@@ -13,6 +13,7 @@ function c11528685.initial_effect(c)
 	e2:SetDescription(aux.Stringid(11528685,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCode(EVENT_TO_GRAVE)
 	e2:SetTarget(c11528685.thtg)
 	e2:SetOperation(c11528685.thop)
@@ -27,7 +28,7 @@ function c11528685.thfilter(c)
 	return c:IsSetCard(0x850) and not c:IsCode(11528685) and c:IsAbleToHand()
 end
 function c11528685.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingMatchingCard(c11528685.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function c11528685.thop(e,tp,eg,ep,ev,re,r,rp)
