ec93dab1850fb66f725426fea66be20c945a2192
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2018-03-07 13:30:23 +0100

    Kitseki Nurturesse v2.0

--- expansions/script/c88513898.lua
+++
@@ -1,4 +0,0 @@
---Kitseki Nurturesse Fix
---Script by XGlitchy30
-function c88513898.initial_effect(c)
-end
--- expansions/script/c88523898.lua
+++ expansions/script/c88523898.lua
@@ -1,4 +1,4 @@
---Kitseki Nurturesse
+--Kitseki Nurturesse (UPDATED)
 --Script by XGlitchy30
 function c88523898.initial_effect(c)
 	--link procedure
@@ -20,13 +20,6 @@ function c88523898.initial_effect(c)
 	e2:SetOperation(c88523898.effectop)
 	c:RegisterEffect(e2)
 end
-local pack={}
-	pack[1]={
-		88513898
-	}
-	pack[2]={
-		88523898
-	}
 --filters
 function c88523898.rvfilter1(c,tp)
 	local lv1=c:GetLevel()
@@ -53,81 +46,23 @@ function c88523898.chgcost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c88523898.chgop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local pos=c:GetPosition()
-	local seq=c:GetSequence()
-	local lv=e:GetLabel()
-	local syn=Group.CreateGroup()
-	----------
-	if c:IsFaceup() and not c:IsType(TYPE_SYNCHRO) then
-		Duel.Exile(c,REASON_RULE)
-		local cpack=pack[1]
-		local alias=cpack[math.random(#cpack)]
-		syn:AddCard(Duel.CreateToken(tp,alias))
-	--PREVIOUS VERSION------------------------
-	--	Duel.DisableShuffleCheck()
-	--	Duel.SendtoDeck(syn,nil,0,REASON_RULE)
-	--	Duel.SpecialSummon(syn:GetFirst(),0,tp,tp,true,false,pos)
-	-------------------------------------------
-		Duel.MoveToField(syn:GetFirst(),tp,tp,LOCATION_MZONE,pos,true)
-		Duel.MoveSequence(syn:GetFirst(),seq)
-		syn:GetFirst():RegisterFlagEffect(88523898,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
-		--name change
-		local name=Effect.CreateEffect(c)
-		name:SetType(EFFECT_TYPE_SINGLE)
-		name:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-		name:SetCode(EFFECT_ADD_CODE)
-		name:SetValue(88523898)
-		name:SetReset(RESET_EVENT+0x1fe0000)
-		syn:GetFirst():RegisterEffect(name)
-		--level change
-		local lev=Effect.CreateEffect(c)
-		lev:SetType(EFFECT_TYPE_SINGLE)
-		lev:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-		lev:SetCode(EFFECT_CHANGE_LEVEL)
-		lev:SetValue(lv)
-		lev:SetReset(RESET_EVENT+0x1fe0000)
-		syn:GetFirst():RegisterEffect(lev)
-		--synchro material
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-		e2:SetCode(EVENT_BE_MATERIAL)
-		e2:SetCondition(c88523898.effectcon)
-		e2:SetOperation(c88523898.effectop)
-		syn:GetFirst():RegisterEffect(e2)
-		--copy effect
-		local e1=Effect.CreateEffect(c)
-		e1:SetDescription(aux.Stringid(88523898,0))
-		e1:SetType(EFFECT_TYPE_IGNITION)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetCountLimit(1)
-		e1:SetCondition(c88523898.chgcon_syn)
-		e1:SetCost(c88523898.chgcost)
-		e1:SetOperation(c88523898.chgop)
-		syn:GetFirst():RegisterEffect(e1)
-		--leave field fixes
-		local fx2=Effect.CreateEffect(c)
-		fx2:SetDescription(aux.Stringid(40854197,0))
-		fx2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-		fx2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_UNCOPYABLE)
-		fx2:SetCode(EVENT_TO_GRAVE)
-		fx2:SetOperation(c88523898.reboot)
-		syn:GetFirst():RegisterEffect(fx2)
-		local fx3=fx2:Clone()
-		fx3:SetCode(EVENT_REMOVE)
-		syn:GetFirst():RegisterEffect(fx3)
-		local fx4=fx2:Clone()
-		fx4:SetCode(EVENT_TO_DECK)
-		syn:GetFirst():RegisterEffect(fx4)
-		----------------------------------
-	elseif c:IsFaceup() and c:IsType(TYPE_SYNCHRO) then
-		local levsyn=Effect.CreateEffect(c)
-		levsyn:SetType(EFFECT_TYPE_SINGLE)
-		levsyn:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-		levsyn:SetCode(EFFECT_CHANGE_LEVEL)
-		levsyn:SetValue(lv)
-		levsyn:SetReset(RESET_EVENT+0x1fe0000)
-		c:RegisterEffect(levsyn)
-	else return end
+	Card.SetCardData(c,CARDDATA_TYPE,TYPE_MONSTER+TYPE_SYNCHRO+TYPE_EFFECT)
+	Card.SetCardData(c,CARDDATA_LEVEL,e:GetLabel())
+	Card.SetCardData(c,CARDDATA_DEFENSE,0)
+	--leave field fixes
+	local fx2=Effect.CreateEffect(c)
+	fx2:SetDescription(aux.Stringid(40854197,0))
+	fx2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	fx2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_UNCOPYABLE)
+	fx2:SetCode(EVENT_TO_GRAVE)
+	fx2:SetOperation(c88523898.reboot)
+	c:RegisterEffect(fx2)
+	local fx3=fx2:Clone()
+	fx3:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(fx3)
+	local fx4=fx2:Clone()
+	fx4:SetCode(EVENT_TO_DECK)
+	c:RegisterEffect(fx4)
 end	
 --gain effect
 function c88523898.effectcon(e,tp,eg,ep,ev,re,r,rp)
@@ -163,25 +98,7 @@ end
 --leave field
 function c88523898.reboot(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local loc=c:GetLocation()
-	local pos=c:GetPosition()
-	local rg=Group.CreateGroup()
-	local cpack=pack[2]
-	local original=cpack[math.random(#cpack)]
-	rg:AddCard(Duel.CreateToken(tp,original))
-	local rcd=rg:GetFirst()
-	Duel.Exile(c,REASON_RULE)
-	if loc==LOCATION_GRAVE then
-		Duel.SendtoGrave(rcd,REASON_RULE)
-	elseif loc==LOCATION_REMOVED and pos==POS_FACEUP then
-		Duel.Remove(rcd,POS_FACEUP,REASON_EFFECT)
-	elseif loc==LOCATION_REMOVED and pos==POS_FACEDOWN then
-		Duel.Remove(rcd,POS_FACEDOWN,REASON_EFFECT)
-	elseif loc==LOCATION_EXTRA then
-		Duel.SendtoDeck(rcd,nil,2,REASON_EFFECT)
-	else return end
-end
--- last fixes (FINALLY!!!)
-function c88523898.chgcon_syn(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetFlagEffect(88523898)==0
+	Card.SetCardData(c,CARDDATA_TYPE,TYPE_MONSTER+TYPE_LINK+TYPE_EFFECT)
+	Card.SetCardData(c,CARDDATA_LEVEL,2)
+	Card.SetCardData(c,CARDDATA_LINK_MARKER,LINK_MARKER_BOTTOM_LEFT+LINK_MARKER_BOTTOM_RIGHT)
 end
