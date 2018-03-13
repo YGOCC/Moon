1ce49dfff185069386dc4a437a559878eb986162
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2018-03-13 21:15:15 +0200

    Updated Chad's card

--- expansions/script/c16000892.lua
+++ expansions/script/c16000892.lua
@@ -97,7 +97,8 @@ function c16000892.filter(c,e,tp)
 	return c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c16000892.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition()
 		and Duel.IsExistingMatchingCard(c16000892.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
