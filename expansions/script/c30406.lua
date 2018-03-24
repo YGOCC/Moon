8240e98ce6a43d280dafcbf3e77196756ad4b1cf
senpaizuri3 <senpaizuri3@gmail.com>
2018-03-24 16:41:17 -0400

    Revert "Update ZHERO"

--- expansions/script/c30406.lua
+++ expansions/script/c30406.lua
@@ -12,7 +12,7 @@ local scard,s_id=getID()
 
 function scard.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,Card.IsZHERO,2,2)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_WARRIOR),2,2)
 	--atkchange
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(s_id,0))
@@ -35,23 +35,6 @@ function scard.initial_effect(c)
 	e2:SetTarget(scard.sptg)
 	e2:SetOperation(scard.spop)
 	c:RegisterEffect(e2)
-	if not scard.global_check then
-		scard.global_check=true
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
-		ge2:SetOperation(scard.archchk)
-		Duel.RegisterEffect(ge2,0)
-	end
-end
-function scard.archchk(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(0,30000)==0 then 
-		Duel.CreateToken(tp,30000)
-		Duel.CreateToken(1-tp,30000)
-		Duel.RegisterFlagEffect(0,30000,0,0,0)
-	end
 end
 function scard.cfilter(c,g)
 	return c:IsFaceup() and g:IsContains(c) and c:GetAttack()>0
--- expansions/script/c30409.lua
+++ expansions/script/c30409.lua
@@ -12,7 +12,7 @@ local scard,s_id=getID()
 
 function scard.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,Card.IsZHERO,2,99)
+	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_WARRIOR),2,99)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -31,23 +31,6 @@ function scard.initial_effect(c)
 	e1:SetTarget(scard.sptg)
 	e1:SetOperation(scard.spop)
 	c:RegisterEffect(e1)
-	if not scard.global_check then
-		scard.global_check=true
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
-		ge2:SetOperation(scard.archchk)
-		Duel.RegisterEffect(ge2,0)
-	end
-end
-function scard.archchk(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFlagEffect(0,30000)==0 then 
-		Duel.CreateToken(tp,30000)
-		Duel.CreateToken(1-tp,30000)
-		Duel.RegisterFlagEffect(0,30000,0,0,0)
-	end
 end
 function scard.cfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_TUNER)
