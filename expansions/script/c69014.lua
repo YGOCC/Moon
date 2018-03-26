1300d2661ee6b6b9be85c9a2ca631b02ca152449
senpaizuri3 <senpaizuri3@gmail.com>
2018-03-26 00:01:57 -0400

    Add files via upload

--- expansions/script/c69014.lua
+++ expansions/script/c69014.lua
@@ -22,24 +22,22 @@ function c69014.cfilter(c)
 end
 function c69014.condition(e,tp,eg,ep,ev,re,r,rp)
 	if ep==tp or not Duel.IsExistingMatchingCard(c69014.cfilter,tp,LOCATION_MZONE,0,1,nil) then
-	return Duel.IsChainNegatable(ev) and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE))
-end
+		return Duel.IsChainNegatable(ev) and (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE))
+	end
 end
 function c69014.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(500)
-    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
+		Duel.SetTargetPlayer(1-tp)
 	end
 end
 function c69014.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 		p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-    Duel.Recover(p,d,REASON_EFFECT)
+		Duel.Recover(p,d,REASON_EFFECT)
 	end
 end
 function c69014.filter(c)
