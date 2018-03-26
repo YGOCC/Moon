b676a00f580df3810f00da58352549d2af0c4958
senpaizuri3 <senpaizuri3@gmail.com>
2018-03-26 15:14:18 -0400

    Add files via upload

--- expansions/script/c69002.lua
+++ expansions/script/c69002.lua
@@ -29,50 +29,57 @@ function c69002.initial_effect(c)
 	e3:SetCondition(c69002.drcon)
 	e3:SetTarget(c69002.drtg)
 	e3:SetOperation(c69002.drop)
+	e3:SetCountLimit(1,690002)
 	c:RegisterEffect(e3)
 end
-function c69002.reccon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()==tp
+function
+c69002.reccon(e,tp,eg,ep,ev,re,r,rp)
+return Duel.GetTurnPlayer()==tp
 end
-function c69002.reccost(e,tp,eg,ep,ev,re,r,rp,chk)
+function
+c69002.reccost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return not e:GetHandler():IsPublic() end
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetCode(EFFECT_PUBLIC)
     e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
     e:GetHandler():RegisterEffect(e1)
-end
-function c69002.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+    end
+    function
+    c69002.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return true end
     Duel.SetTargetPlayer(tp)
     Duel.SetTargetParam(500)
     Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
-end
-function c69002.recop(e,tp,eg,ep,ev,re,r,rp)
+    end
+    function
+    c69002.recop(e,tp,eg,ep,ev,re,r,rp)
     local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
     Duel.Recover(p,d,REASON_EFFECT)
-end
-function c69002.spcon(e,c)
+    function c69002.spcon(e,c)
     if c==nil then return true end
     local tp=c:GetControler()
-    return Duel.GetLP(tp)>=Duel.GetLP(1-tp) and Duel.IsExistingMatchingCard(c69002.filter1,tp,LOCATION_HAND,0,1,e:GetHandler())
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+    return Duel.GetLP(tp)>=Duel.GetLP(1-tp)
 end
-function c69002.filter(c)
-	return c:IsPublic() and c:IsSetCard(0x6969)
 end
-function c69002.drcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+function c69002.filter (c)
+return c:IsPublic() and c:IsSetCard(0x6969)
 end
-function c69002.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+function c69002.spcon (e,c)
+  return Duel.IsExistingMatchingCard(c69002.filter1,tp,LOCATION_HAND,0,1,e:GetHandler()) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+ end
+ function c69002.drcon (e,tp,eg,ep,ev,re,r,rp)
+ return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+end
+function c69002.drtg (e,tp,eg,ep,ev,re,r,rp,chk)
+if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function c69002.drop(e,tp,eg,ep,ev,re,r,rp)
-	local p1=Duel.GetLP(tp)
-	local p2=Duel.GetLP(1-tp)
-	local s=p2-p1
-	if s<0 then s=p1-p2 end
-	local d=math.floor(s/2000)
-	Duel.Draw(tp,d,REASON_EFFECT)
+function c69002.drop (e,tp,eg,ep,ev,re,r,rp)
+local p1=Duel.GetLP(tp)
+local p2=Duel.GetLP(1-tp)
+local s=p2-p1
+if s<0 then s=p1-p2 end
+local d=math.floor(s/4000)
+Duel.Draw(tp,d,REASON_EFFECT)
 end
--- expansions/script/c69011.lua
+++ expansions/script/c69011.lua
@@ -18,7 +18,7 @@ function c69011.initial_effect(c)
 	e2:SetCost(aux.bfgcost)
 	e2:SetTarget(c69011.thtg)
 	e2:SetOperation(c69011.thop)
-	e2:SetCountLimit(1,69011)
+	e2:SetCountLimit(1,690011)
 	c:RegisterEffect(e2)
  end
 function c69011.filter1(c)
