a23dab510d3f4b23896530862e730cc3cb564565
Lyris12 <lyris89@live.com>
2018-03-28 06:01:17 -0400

    Fixed "Pandemoniumgraph Magician"
    Was using old Pandemonium Procedure with the Token

--- expansions/script/c90239650.lua
+++ expansions/script/c90239650.lua
@@ -1,51 +1,52 @@
 --Pandemoniumgraph Magician
-function c90239650.initial_effect(c)
-	--Protect myserty
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(90239650,2))
-	e1:SetRange(LOCATION_SZONE)
+local card = c90239650
+function card.initial_effect(c)
+--special summon
+	aux.AddOrigPandemoniumType(c)
+	aux.EnablePandemoniumAttribute(c,e1)
+local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetDescription(aux.Stringid(90239650,1))
+	e1:SetRange(LOCATION_SZONE+LOCATION_PZONE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetCondition(c90239650.condition)
-	e1:SetTarget(c90239650.target)
-	e1:SetOperation(c90239650.activate)
+	e1:SetCondition(card.condition)
+	e1:SetTarget(card.qtg)
+	e1:SetOperation(card.qop)
 	c:RegisterEffect(e1)
---Pandemonium
-   if not c90239650.global_check then
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetLabel(326)
-		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
-		ge2:SetOperation(c90239650.chk)
-		Duel.RegisterEffect(ge2,0)
+		--turn set
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(90239650,2))
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_SZONE+LOCATION_PZONE)
+	e3:SetTarget(card.postg)
+	e3:SetOperation(card.posop)
+	c:RegisterEffect(e3)
+end
+function card.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanTurnSet() end
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,c,1,0,0)
+end
+function card.posop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
 	end
 end
-c90239650.pandemonium=true
-c90239650.pandemonium_lscale=8 --x = Left Pandemonium Scale
-c90239650.pandemonium_rscale=1 --y = Right Pandemonium Scale
-function c90239650.chk(e,tp,eg,ep,ev,re,r,rp)
-	Duel.CreateToken(tp,e:GetLabel())
-	Duel.CreateToken(1-tp,e:GetLabel())
+
+function card.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
 end
-function c90239650.condition(e,tp,eg,ep,ev,re,r,rp)
-	return tp~=Duel.GetTurnPlayer()
-end
-function c90239650.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function card.qtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
 	local tg=Duel.GetAttacker()
 	if chkc then return chkc==tg end
 	if chk==0 then return tg:IsOnField() and tg:IsCanBeEffectTarget(e) end
 	Duel.SetTargetCard(tg)
 end
-function c90239650.activate(e,tp,eg,ep,ev,re,r,rp)
+function card.qop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.NegateAttack()
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsCanTurnSet() and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		Duel.BreakEffect()
-		c:CancelToGrave()
-		Duel.ChangePosition(c,POS_FACEDOWN)
-		Duel.RaiseEvent(c,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
-	end
 end
