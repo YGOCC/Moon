fb1b1cc272107889504ba5cd9769b7946a008037
MoonBurst <Moonburstplays@gmail.com>
2018-04-05 14:13:59 -0500

    Add files via upload

--- expansions/script/c210424273.lua
+++ expansions/script/c210424273.lua
@@ -1,33 +1,40 @@
---Moon Burst: Lunar Cycles
+--Moon Burst's Second Chance
 local card = c210424273
 function card.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(card.qcost)
-	e1:SetTarget(card.target)
-	e1:SetOperation(card.activate)
+	e1:SetCountLimit(1,210424280)
+	e1:SetTarget(card.sumtg)
+	e1:SetOperation(card.sumop)
 	c:RegisterEffect(e1)
 	--sp summon, banish when leaving field
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,210424280)
+	e2:SetCountLimit(1,210424281)
 	e2:SetCondition(aux.exccon)
 	e2:SetTarget(card.sptg)
 	e2:SetOperation(card.spop)
 	c:RegisterEffect(e2)
 end
-
+function card.tgfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x666) and c:IsType(TYPE_MONSTER)
+end
 function card.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:GetFlagEffect(210424273)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,210424273,0x666,0,1500,1500,4,RACE_WINDBEAST,ATTRIBUTE_WIND) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and card.tgfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(card.tgfilter,tp,LOCATION_MZONE,0,1,nil) 
+	and c:GetFlagEffect(210424273)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	and Duel.IsPlayerCanSpecialSummonMonster(tp,210424273,0x666,0,1500,1500,4,RACE_WINDBEAST,ATTRIBUTE_WIND) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,card.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	c:RegisterFlagEffect(210424273,RESET_CHAIN,0,1)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
@@ -48,23 +55,30 @@ function card.spop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummonComplete()
 	end
 end
-
-function card.filter(c)
-	return c:IsSetCard(0x666) and c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:IsType(TYPE_PENDULUM)
+function card.spfilter(c,e,tp)
+	return c:IsSetCard(0x666) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function card.qcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(card.filter,tp,LOCATION_EXTRA,0,1,nil,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local c=Duel.SelectMatchingCard(tp,card.filter,tp,LOCATION_EXTRA,0,1,1,nil,c)
-	Duel.SendtoGrave(c,REASON_COST)
+function card.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(card.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function card.sumop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,card.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+0x1fe0000)
+		tc:RegisterEffect(e2)
 	end
-function card.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(2)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
-end
-function card.activate(e,tp,eg,ep,ev,re,r,rp)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
 end
