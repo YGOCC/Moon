c84593b6a7cb69bd4719e6467b229c4fb133030f
Lyris12 <Lyris12@users.noreply.github.com>
2018-04-07 06:10:30 -0400

    Revert "New spell changed, since it was largely disliked by other players"

--- expansions/script/c210424273.lua
+++ expansions/script/c210424273.lua
@@ -1,84 +1,58 @@
---Moon Burst's Second Chance
+--Moon Burst: Lunar Cycles
 local card = c210424273
 function card.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,210424280)
-	e1:SetTarget(card.sumtg)
-	e1:SetOperation(card.sumop)
+	e1:SetTarget(card.target)
+	e1:SetOperation(card.activate)
 	c:RegisterEffect(e1)
-	--sp summon, banish when leaving field
+	--mill 2; send to deck
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetCategory(CATEGORY_TODECK)
 	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,210424281)
+	e2:SetCountLimit(1,210424280)
 	e2:SetCondition(aux.exccon)
-	e2:SetTarget(card.sptg)
-	e2:SetOperation(card.spop)
+	e2:SetCost(card.qcost)
+	e2:SetTarget(card.rettg)
+	e2:SetOperation(card.rtdop)
 	c:RegisterEffect(e2)
 end
-function card.tgfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x666) and c:IsType(TYPE_MONSTER)
+
+function card.qcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,2) end
+	Duel.DiscardDeck(tp,2,REASON_COST)
 end
-function card.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and card.tgfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(card.tgfilter,tp,LOCATION_MZONE,0,1,nil) 
-	and c:GetFlagEffect(210424273)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	and Duel.IsPlayerCanSpecialSummonMonster(tp,210424273,0x666,0,1500,1500,4,RACE_WINDBEAST,ATTRIBUTE_WIND) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,card.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	c:RegisterFlagEffect(210424273,RESET_CHAIN,0,1)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+function card.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
 end
-function card.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+function card.rtdop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.IsPlayerCanSpecialSummonMonster(tp,210424273,0x666,0,1500,1500,4,RACE_WINDBEAST,ATTRIBUTE_WIND) then
-		c:AddMonsterAttribute(TYPE_NORMAL)
-		Duel.SpecialSummonStep(c,0,tp,tp,true,false,POS_FACEUP)
-		c:AddMonsterAttributeComplete()
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
-		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e2:SetReset(RESET_EVENT+0x47e0000)
-		e2:SetValue(LOCATION_REMOVED)
-		c:RegisterEffect(e2,true)
-		Duel.SpecialSummonComplete()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
 	end
 end
-function card.spfilter(c,e,tp)
-	return c:IsSetCard(0x666) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function card.filter(c)
+	return c:IsSetCard(0x666) 
 end
-function card.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(card.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+function card.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp)
+		and Duel.IsExistingMatchingCard(card.filter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,1,tp,LOCATION_HAND)
 end
-function card.sumop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,card.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	local tc=g:GetFirst()
-	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e1)
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		e2:SetReset(RESET_EVENT+0x1fe0000)
-		tc:RegisterEffect(e2)
+function card.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_DISCARD)
+		local ct=Duel.DiscardHand(tp,card.filter,1,99,REASON_EFFECT+REASON_DISCARD)
+		Duel.BreakEffect()
+		Duel.Draw(p,ct+1,REASON_EFFECT)
 	end
-end
+	
