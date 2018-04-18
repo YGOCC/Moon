4f3ef47b960468e3f798601996dbb635d4bae2e9
senpaizuri3 <senpaizuri3@gmail.com>
2018-04-18 13:04:12 -0400

    Update Signer Red

--- expansions/script/c101600105.lua
+++ expansions/script/c101600105.lua
@@ -87,7 +87,8 @@ function c101600105.filter(c,e,tp)
 		and Duel.IsExistingMatchingCard(c101600105.exfilter,tp,LOCATION_EXTRA,0,1,nil,lv+lv2,e,tp)
 end
 function c101600105.exfilter(c,lv,e,tp)
-	return c:GetLevel()==lv and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false) and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_SYNCHRO)
+	return c:GetLevel()==lv and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false) and c:IsRace(RACE_DRAGON) and c:IsSetCard(0xcd01)
+		and c:IsType(TYPE_SYNCHRO)
 end
 function c101600105.ex(c,tc)
 	return c:GetSequence()==4 or c:GetSequence()==5 and c:GetLinkedGroup():IsContains(tc)
