6a925510b63e6f80d7fe8a19eb3904595d900bb3
Lyris12 <lyris89@live.com>
2018-04-28 12:39:07 -0400

    Tweaked "Fusion Exboom" by Lyris
    * Is no longer restricted to Machine monsters

--- expansions/script/c240100055.lua
+++ expansions/script/c240100055.lua
@@ -14,7 +14,7 @@ function c240100055.filter1(c,e)
 	return not c:IsImmuneToEffect(e)
 end
 function c240100055.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and c:IsRace(RACE_MACHINE) and (not f or f(c))
+	return c:IsType(TYPE_FUSION) and (not f or f(c))
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
 end
 function c240100055.target(e,tp,eg,ep,ev,re,r,rp,chk)
