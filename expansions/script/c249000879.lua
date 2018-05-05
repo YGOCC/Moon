0a9aa4f47cb8ed6419237434f6944c2d89522059
YGOCC <chadoook1990@gmail.com>
2018-05-05 01:27:30 +0100

    More updates for Somen00b

--- expansions/script/c249000879.lua
+++ expansions/script/c249000879.lua
@@ -1,55 +1,63 @@
---SC2 Unit - Phoenix
-xpcall(function() require("expansions/script/c37564765") end,function() require("script/c37564765") end)
-function c115000261.initial_effect(c)
-	Senya.AddSummonSE(c,aux.Stringid(115000261,0))
-	Senya.AddAttackSE(c,aux.Stringid(115000261,1))
+--Alolan Ninetales, Emissary of Ice
+function c249000879.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(c115000261.spcon)
+	e1:SetCondition(c249000879.spcon)
 	c:RegisterEffect(e1)
-	--grav beam
+	--freeze
 	local e2=Effect.CreateEffect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_NO_TURN_RESET)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1)
-	e2:SetTarget(c115000261.target)
-	e2:SetOperation(c115000261.operation)
+	e2:SetTarget(c249000879.target)
+	e2:SetOperation(c249000879.operation)
 	c:RegisterEffect(e2)
 end
-function c115000261.spcon(e,c)
+function c249000879.spcon(e,c)
 	if c==nil then return true end
 	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0)==0
-		and	Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE)>0
 		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
-function c115000261.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c249000879.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsFaceup() and chkc:IsLocation(LOCATION_MZONE) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
 end
-function c115000261.operation(e,tp,eg,ep,ev,re,r,rp)
+function c249000879.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CANNOT_TRIGGER)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,2)
 		tc:RegisterEffect(e1)
 		local e3=e1:Clone()
-		e3:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
 		tc:RegisterEffect(e3)
 		local e4=e1:Clone()
 		e4:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
 		tc:RegisterEffect(e4)
-		local e5=e4:Clone()
-		e5:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
-		tc:RegisterEffect(e5)
+		local e5=Effect.CreateEffect(c)
+		e5:SetType(EFFECT_TYPE_SINGLE)
+		e5:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+		e5:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,2)
+		e5:SetValue(1)
+		c:RegisterEffect(e5)
+		local e6=e5:Clone()
+		e6:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+		tc:RegisterEffect(e6)
+		local e7=e5:Clone()
+		e7:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+		tc:RegisterEffect(e7)
+		local e8=e5:Clone()
+		e8:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+		tc:RegisterEffect(e8)
 	end
 end
