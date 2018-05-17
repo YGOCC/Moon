95bf5368fad7aff564e28f0e37b407409cc60a12
Lyris12 <lyris89@live.com>
2018-05-17 07:39:00 -0400

    Added On-Self-Summon Draw effect to "Newtrix" Main Deck monsters by Lyris

--- expansions/script/c240100175.lua
+++ expansions/script/c240100175.lua
@@ -11,7 +11,7 @@ function c240100175.initial_effect(c)
 	c:RegisterEffect(e1)
 	local o1=e1:Clone()
 	o1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	o1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_DAMAGE_STEP)
+	o1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	o1:SetCondition(c240100175.ocon(c240100175.modcon))
 	c:RegisterEffect(o1)
 	local e2=Effect.CreateEffect(c)
@@ -22,6 +22,21 @@ function c240100175.initial_effect(c)
 	e2:SetCondition(c240100175.spcon)
 	e2:SetValue(c240100175.spval)
 	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCountLimit(1,240100175)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetDescription(1108)
+	e3:SetCondition(c240100175.mcon(c240100175.drcon))
+	e3:SetTarget(c240100175.drtg)
+	e3:SetOperation(c240100175.drop)
+	c:RegisterEffect(e3)
+	local o3=e3:Clone()
+	o3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	o3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_DELAY)
+	o3:SetCondition(c240100175.ocon(c240100175.drcon))
+	c:RegisterEffect(o3)
 end
 function c240100175.spcon(e,c)
 	if c==nil then return true end
@@ -41,7 +56,7 @@ function c240100175.spcon(e,c)
 	return Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)>0
 end
 function c240100175.spval(e,c)
-	return 0,e:GetLabel()
+	return 1,e:GetLabel()
 end
 function c240100175.modcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,LOCATION_MZONE,1,eg,TYPE_LINK) and (Duel.GetCurrentPhase()~=PHASE_DAMAGE or (not Duel.IsDamageCalculated() and (e:IsHasType(EFFECT_TYPE_TRIGGER_F) or (eg:GetCount()==1 and eg:IsContains(e:GetHandler())))))
@@ -101,19 +116,16 @@ function c240100175.lmval(t)
 				return chgMark
 			end
 end
-function c240100175.filter(c)
-	return c:GetLevel()==4 and c:IsRace(RACE_CYBERS) and c:IsAbleToHand() and not c:IsCode(240100175)
+function c240100175.drcon(e)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1
 end
-function c240100175.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:IsHasType(EFFECT_TYPE_TRIGGER_F)
-		or Duel.IsExistingMatchingCard(c240100175.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function c240100175.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_TRIGGER_F) or Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
-function c240100175.operation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c240100175.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if g:GetCount()>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
-	end
+function c240100175.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
