03fdd7feb341bcf3bf1494e573bb2f35330c7fa5
KServantes <elitalianoverde@gmail.com>
2018-06-03 15:11:50 -0600

    Update Pendulum Advance
    Balancing

--- expansions/script/c10130500.lua
+++ expansions/script/c10130500.lua
@@ -15,36 +15,35 @@ function cod.initial_effect(c)
 end
 function cod.cfilter(c)
 	local scale=0
-	if c:GetSequence()==6 then
+	if c:GetSequence()==0 then
 		scale=c:GetLeftScale()
 	else
 		scale=c:GetRightScale()
 	end
-	return c:IsLocation(LOCATION_SZONE) and c:IsType(TYPE_PENDULUM) and c:IsAbleToDeck()
-		and Duel.IsExistingMatchingCard(cod.scfilter,c:GetControler(),LOCATION_DECK,0,1,nil,scale)
+	return c:IsAbleToDeck() and Duel.IsExistingMatchingCard(cod.scfilter,c:GetControler(),LOCATION_DECK,0,1,nil,scale,c:GetCode())
 end
-function cod.scfilter(c,scale)
-	return c:IsType(TYPE_PENDULUM) and (c:GetLeftScale()==scale or c:GetRightScale()==scale) and not c:IsForbidden()
+function cod.scfilter(c,scale,code)
+	return c:IsType(TYPE_PENDULUM) and c:GetCode()~=code and (c:GetLeftScale()==scale or c:GetRightScale()==scale) and not c:IsForbidden()
 end
 function cod.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_SZONE) and c:IsControler(tp) and cod.cfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(cod.cfilter,tp,LOCATION_SZONE,0,1,nil) end
+	if chkc then return chkc:IsLocation(LOCATION_PZONE) and chkc:IsControler(tp) and chkc:IsAbleToDeck() end
+	if chk==0 then return Duel.IsExistingTarget(cod.cfilter,tp,LOCATION_PZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,cod.cfilter,tp,LOCATION_SZONE,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,cod.cfilter,tp,LOCATION_PZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
 end
 function cod.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	local scale=0
-	local seq=tc:GetSequence()
-	if seq==6 then
+	local seq,code=tc:GetSequence(),tc:GetCode()
+	if seq==0 then
 		scale=tc:GetLeftScale()
 	else
 		scale=tc:GetRightScale()
 	end
-	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)~=0 and Duel.CheckLocation(tp,LOCATION_SZONE,seq) then
+	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)~=0 and Duel.CheckLocation(tp,LOCATION_PZONE,seq) then
 		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-		local g=Duel.SelectMatchingCard(tp,cod.scfilter,tp,LOCATION_DECK,0,1,1,nil,scale)
+		local g=Duel.SelectMatchingCard(tp,cod.scfilter,tp,LOCATION_DECK,0,1,1,nil,scale,code)
 		if g:GetCount()>0 then
 			local c=g:GetFirst()
 			Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
