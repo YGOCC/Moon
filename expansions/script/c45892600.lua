9d877c064528bf9782794dae314e2be6bf7dc5cd
KServantes <elitalianoverde@gmail.com>
2018-06-21 00:08:43 -0600

    1st of Rites
    Archetype by Ace.

---
+++ expansions/script/c45892600.lua
@@ -0,0 +1,140 @@
+--Executor of Rites
+--Concept by Ace
+--Kedy was here~
+local function ID()
+	local str=string.match(debug.getinfo(2,'S')['source'],"c%d+%.lua")
+	str=string.sub(str,1,string.len(str)-4)
+	local cod=_G[str]
+	local id=tonumber(string.sub(str,2))
+	return id,cod
+end
+
+local id,cod=ID()
+function cod.initial_effect(c)
+	--SPSummon Proc
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetValue(1)
+	e1:SetCondition(cod.spcon)
+	e1:SetOperation(cod.spop)
+	c:RegisterEffect(e1)
+	--SPSummon GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(cod.spgcon)
+	e2:SetTarget(cod.spgtg)
+	e2:SetOperation(cod.spgop)
+	c:RegisterEffect(e2)
+	--Search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,id)
+	e3:SetTarget(cod.thtg)
+	e3:SetOperation(cod.thop)
+	c:RegisterEffect(e3)
+end
+
+--Special Summon Proc
+function cod.rmfilter(c)
+	return c:IsAbleToRemove() and c:GetType()&0x82==0x82
+end
+function cod.rsfilter(c)
+	return c:IsAbleToRemove() and c:GetType()&0x81==0x81
+end
+function cod.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(cod.rmfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(cod.rsfilter,tp,LOCATION_GRAVE,0,1,nil)
+end
+function cod.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectMatchingCard(tp,cod.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g2=Duel.SelectMatchingCard(tp,cod.rsfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g1+g2,POS_FACEUP,REASON_COST)
+end
+
+--Special Summon from GY
+function cod.spgcon(e)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+1 and Duel.GetFlagEffect(tp,id)==0
+end
+function cod.spfilter(c,e,tp)
+	return c:GetType()&0x81==0x81 and c:IsCanBeSpecialSummoned(e,0,tp,true,false,POS_FACEUP_DEFENSE,tp)
+end
+function cod.spgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cod.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(cod.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,cod.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,tp,LOCATION_GRAVE)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+end
+function cod.spgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP_DEFENSE) then
+		tc:RegisterFlagEffect(id,RESET_EVENT+0x1fe0000,0,1,Duel.GetTurnCount())
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetLabel(Duel.GetTurnCount())
+		e1:SetLabelObject(tc)
+		e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		e1:SetCondition(cod.rthcon)
+		e1:SetOperation(cod.rthop)
+		Duel.RegisterEffect(e1,tp)
+		Duel.SpecialSummonComplete()
+	end
+end
+function cod.rthcon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	return Duel.GetTurnCount()~=e:GetLabel() and Duel.GetTurnPlayer()~=tp and tc:GetFlagEffectLabel(id)==e:GetLabel()
+end
+function cod.rthop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetLabelObject()
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.SendtoHand(tc,nil,REASON_EFFECT)
+end
+
+--Search
+function cod.thfilter(c)
+	return c:IsType(TYPE_RITUAL) and c:IsAbleToHand() --and not c:IsType(TYPE_TRAP)
+end
+function cod.thtg(e,tp,eg,ep,ev,re,rp,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove()
+		and Duel.IsExistingTarget(cod.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,cod.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,tp,LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,tp,LOCATION_GRAVE)
+end
+function cod.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.Remove(c,POS_FACEUP,REASON_EFFECT) then
+		local tc=Duel.GetFirstTarget()
+		if tc:IsRelateToEffect(e) then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		end
+	end
+end
