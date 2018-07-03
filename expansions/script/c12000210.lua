3a994051cada7d8a5224083db7ab45da5553ffda
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2018-07-03 17:45:59 +0300

    Dimension Dragon fix

--- expansions/script/c12000210.lua
+++ expansions/script/c12000210.lua
@@ -49,15 +49,12 @@ function c12000210.activate(e,tp,eg,ep,ev,re,r,rp)
 end
 function c12000210.cfilter(c)
 	return (c:IsSetCard(0x855) and c:IsDiscardable() and c:IsLocation(LOCATION_HAND))
-		or Duel.IsExistingMatchingCard(c12000210.dfilter,tp,LOCATION_ONFIELD,0,1,nil)
+		or (c:IsSetCard(0x855) and c:IsFaceup() and c:IsAbleToGraveAsCost())
 end
-function c12000210.dfilter(c)
-	return c:IsSetCard(0x855) and c:IsFaceup() and c:IsAbleToGraveAsCost()
-		and not c:IsCode(12000210)
-end
+
 function c12000210.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c12000210.cfilter,tp,LOCATION_HAND,0,1,nil) end
-	local sg=Duel.SelectMatchingCard(tp,c12000210.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,nil,tp)
+	if chk==0 then return Duel.IsExistingMatchingCard(c12000210.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,e:GetHandler()) end
+	local sg=Duel.SelectMatchingCard(tp,c12000210.cfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,e:GetHandler(),tp)
 	if sg:GetFirst():IsLocation(LOCATION_HAND) then
 		Duel.SendtoGrave(sg,REASON_COST+REASON_DISCARD)
 	else
