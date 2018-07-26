adfd72493d0667610b755cb486fd1a1b075267f7
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2018-07-26 18:00:06 +0300

    Taobgut fix

--- expansions/script/c210001102.lua
+++ expansions/script/c210001102.lua
@@ -94,4 +94,5 @@ function c210001102.recon(e)
 	local c=e:GetHandler()
 	return c:IsLocation(LOCATION_MZONE) and (c:IsReason(REASON_BATTLE) 
 		or c:IsReason(REASON_EFFECT)) and e:GetHandler():GetFlagEffect(210001102)~=0
+		and c:GetDestination()==LOCATION_GRAVE
 end
