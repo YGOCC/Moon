db57e6f198673d9ccb265899ccc917f18ab412d9
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2018-07-31 16:46:44 +0300

    Teutonic Umbra fix and added Teutonic Royal Guard

--- expansions/script/c12000125.lua
+++ expansions/script/c12000125.lua
@@ -15,18 +15,18 @@ local e1=Effect.CreateEffect(c)
 	e1:SetOperation(c12000125.spop)
 	c:RegisterEffect(e1)
 --Link Summon
-local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(12000125,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,12000225)
-	e2:SetCondition(c12000125.rmcon)
-	e2:SetTarget(c12000125.linktg)
-	e2:SetOperation(c12000125.linkop)
-	c:RegisterEffect(e2)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(12000125,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+    e2:SetCountLimit(1,12000225)
+    e2:SetCondition(c12000125.rmcon)
+    e2:SetTarget(c12000125.linktg)
+    e2:SetOperation(c12000125.linkop)
+    c:RegisterEffect(e2)
 end
 --Special Summon from hand
 function c12000125.spcon(e,tp,eg,ep,ev,re,r,rp)
@@ -55,12 +55,21 @@ function c12000125.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 --Link Summon
 function c12000125.rmcon(e,tp,eg,ep,ev,re,r,rp)
-	return re and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x857) or re:GetHandler():IsSetCard(0x858) and not re:GetHandler():IsCode(12000125)
+    return re and re:IsActiveType(TYPE_MONSTER) and (re:GetHandler():IsSetCard(0x857) or re:GetHandler():IsSetCard(0x858)) and not re:GetHandler():IsCode(12000125)
 end
 function c12000125.linkfilter(c)
-    return c:IsSpecialSummonable(SUMMON_TYPE_LINK)
+    return c:IsSpecialSummonable(SUMMON_TYPE_LINK) and c:IsSetCard(0x857)
 end
 function c12000125.linktg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(c12000125.linkfilter,tp,LOCATION_EXTRA,0,1,nil,e:GetHandler()) end
+    if chk==0 then return Duel.IsExistingMatchingCard(c12000125.linkfilter,tp,LOCATION_EXTRA,0,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function c12000125.linkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:GetControler()~=tp or not c:IsRelateToEffect(e) then return end
+    local g=Duel.SelectMatchingCard(tp,c12000125.linkfilter,tp,LOCATION_EXTRA,0,1,1,nil,c)
+    local tc=g:GetFirst()
+    if tc then
+        Duel.SpecialSummonRule(tp,tc,SUMMON_TYPE_LINK,c)   
+    end
 end
