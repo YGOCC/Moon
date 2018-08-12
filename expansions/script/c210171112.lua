8046afae31d4ca691d9095b6280f6d4b7853c607
Terrie <32037239+Terrieforming@users.noreply.github.com>
2018-08-13 02:50:31 +1000

    Update Nekroz

--- expansions/script/c210171112.lua
+++ expansions/script/c210171112.lua
@@ -52,31 +52,20 @@ function c210171112.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c210171112.atkfilter(c)
-	return c:IsFaceup() and c:GetSummonLocation()==LOCATION_EXTRA
+	return c:GetSequence()>4
 end
 function c210171112.atkfilter2(c)
-	return c:IsSetCard(0xb4) and c:IsAbleToHand()
+	return c:IsLocation(LOCATION_ONFIELD) or (c:IsSetCard(0xb4) and c:IsLocation(LOCATION_GRAVE)) and c:IsAbleToHand()
 end
 function c210171112.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local ct=math.min(Duel.GetMatchingGroupCount(c210171112.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil),2)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE+LOCATION_GRAVE) and c210171112.atkfilter2(chkc) end
-	if chk==0 then return ct>0 and Duel.IsExistingTarget(c210171112.atkfilter2,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,e:GetHandler()) end
+	local ct=Duel.GetMatchingGroupCount(c210171112.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD+LOCATION_GRAVE) and c210171112.atkfilter2(chkc) end
+	if chk==0 then return ct>0 and Duel.IsExistingTarget(c210171112.atkfilter2,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g=Duel.SelectTarget(tp,c210171112.atkfilter2,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,ct,e:GetHandler())
+	local g=Duel.SelectTarget(tp,c210171112.atkfilter2,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,ct,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
 end
 function c210171112.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
 	Duel.SendtoHand(g,nil,REASON_EFFECT)
-	local og=Duel.GetOperatedGroup()
-	local ct=og:FilterCount(Card.IsLocation,nil,LOCATION_HAND)
-	local c=e:GetHandler()
-	if ct>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+0x1ff0000)
-		e1:SetValue(ct*500)
-		c:RegisterEffect(e1)
-	end
 end
