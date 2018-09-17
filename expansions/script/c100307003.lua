edc617932d355fd980957b7102b13f22ce5d7f0e
KServantes <elitalianoverde@gmail.com>
2018-09-16 22:03:52 -0600

    Pre-Release Update
    Added
    ==SAST==
    09 - T.G. Screw Serpent (101007009)
    18 - Shiranui Sensei (101007018)
    19 - Shiranui Spearitualist (101007019)
    37 - Borreload Savage Dragon (101007037)
    38 - Cyberse Quantum Dragon (101007038)
    41 - Shiranui Spiritsaga (101007041)
    48 - Salamangreat Sunlight Wolf (101007048)
    50 - T.G. Trident Launcher (101007050)
    54 - Shiranui Splendidsaga (101007054)
    63 - Shiranui Story Saga (101007063)
    67 - Pot of Indulgence (101007067)
    74 - Shiranui Style Skill (101007074)
    78 - Dark Factory of Increased Production (101007078)
    79 - Witch's Strike (101007079)
    
    ==DP11==
    01 - Harpie Perfumer (100411001)
    02 - Harpie Oracle (100411002)
    03 - Split Mirrors of Seduction (100411003)
    05 - The Elegant Harpie Lady (100411005)

--- expansions/script/c100307003.lua
+++ expansions/script/c100307003.lua
@@ -1,6 +1,6 @@
 --グローアップ・ブルーム
 --Glow-Up Bloom
---Scripted by Eerie Code
+--Scripted by Eerie Code, mod by mercury233
 function c100307003.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -14,22 +14,30 @@ function c100307003.initial_effect(c)
 	e1:SetOperation(c100307003.operation)
 	c:RegisterEffect(e1)
 end
-c100307003.listed_names={4064256}
 function c100307003.filter(c,e,tp,chk)
-	return c:IsLevelAbove(5) and c:IsRace(RACE_ZOMBIE) and (c:IsAbleToHand() or (chk and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)))
-end
-function c100307003.fieldcond(c)
-	return c:IsFaceup() and c:IsCode(4064256)
+	return c:IsLevelAbove(5) and c:IsRace(RACE_ZOMBIE)
+		and (c:IsAbleToHand() or chk and c:IsCanBeSpecialSummoned(e,0,tp,false,false))
 end
 function c100307003.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local ck=Duel.IsExistingMatchingCard(c100307003.fieldcond,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil)
-		return Duel.IsExistingMatchingCard(c100307003.filter,tp,LOCATION_DECK,0,1,nil,e,tp,ck)
+		local res=Duel.IsEnvironment(4064256,PLAYER_ALL,LOCATION_FZONE) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		return Duel.IsExistingMatchingCard(c100307003.filter,tp,LOCATION_DECK,0,1,nil,e,tp,res)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,0,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,0,tp,LOCATION_DECK)
 end
 function c100307003.operation(e,tp,eg,ep,ev,re,r,rp)
+	local res=Duel.IsEnvironment(4064256,PLAYER_ALL,LOCATION_FZONE) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tc=Duel.SelectMatchingCard(tp,c100307003.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp,res):GetFirst()
+	if tc then
+		if res and (not tc:IsAbleToHand() or Duel.SelectOption(tp,1190,1152)==1) then
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		else
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		end
+	end
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -39,17 +47,6 @@ function c100307003.operation(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTarget(c100307003.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
-	local ck=Duel.IsExistingMatchingCard(c100307003.fieldcond,tp,LOCATION_FZONE,LOCATION_FZONE,1,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local tc=Duel.SelectMatchingCard(tp,c100307003.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp,ck):GetFirst()
-	if tc then
-		if ck and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and (not tc:IsAbleToHand() or Duel.SelectYesNo(tp,aux.Stringid(100307003,1))) then
-			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-		else
-			Duel.SendtoHand(tc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
-		end
-	end
 end
 function c100307003.splimit(e,c,sump,sumtype,sumpos,targetp,se)
 	return not c:IsRace(RACE_ZOMBIE)
--- expansions/script/c100307024.lua
+++ expansions/script/c100307024.lua
@@ -1,20 +1,23 @@
+--アンデット・ストラグル
 --Zombie Struggle
---Script by Kedy
+--Logical Nonsense, mod by mercury233
 function c100307024.initial_effect(c)
-	--activate
-	local  e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100307024,0))
+	--modify ATK
+	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetDescription(aux.Stringid(100307024,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCondition(c100307024.condition)
 	e1:SetTarget(c100307024.target)
 	e1:SetOperation(c100307024.activate)
 	c:RegisterEffect(e1)
-	--set
+	--Shuffle to set from GY
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100307024,1))
 	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetDescription(aux.Stringid(100307024,3))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetCountLimit(1,100307024)
@@ -22,48 +25,43 @@ function c100307024.initial_effect(c)
 	e2:SetOperation(c100307024.setop)
 	c:RegisterEffect(e2)
 end
+function c100307024.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
 function c100307024.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_ZOMBIE)
 end
-function c100307024.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100307024.cfilter(chkc) end
+function c100307024.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc,race)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c100307024.filter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(c100307024.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,c100307024.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	local tc=g:GetFirst()
-	local op=0
-	if tc:GetAttack()==0 then op=Duel.SelectOption(tp,aux.Stringid(100307024,1))
-	else op=Duel.SelectOption(tp,aux.Stringid(100307024,1),aux.Stringid(100307024,2)) end
-	e:SetLabel(op)
-	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,g:GetCount(),0,0)
+	Duel.SelectTarget(tp,c100307024.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
 function c100307024.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local atk=1000
+		if Duel.SelectOption(tp,aux.Stringid(100307024,1),aux.Stringid(100307024,2))==1 then atk=-1000 end 
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		if e:GetLabel()==0 then
-			e1:SetValue(1000)
-		else e1:SetValue(-1000) end
+		e1:SetCode(EFFECT_UPDATE_ATTACK) 
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END) 
+		e1:SetValue(atk)
 		tc:RegisterEffect(e1)
 	end
 end
-function c100307024.setfilter(c)
+function c100307024.retfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_ZOMBIE) and c:IsAbleToDeck()
 end
 function c100307024.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsSSetable()
-		and Duel.IsExistingMatchingCard(c100307024.setfilter,tp,LOCATION_REMOVED,0,1,nil) end
+		and Duel.IsExistingMatchingCard(c100307024.retfilter,tp,LOCATION_REMOVED,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
 end
 function c100307024.setop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,c100307024.setfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c100307024.retfilter,tp,LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 and Duel.SendtoDeck(g,nil,2,REASON_EFFECT)>0
 		and c:IsRelateToEffect(e) and c:IsSSetable() then
 		Duel.SSet(tp,c)
@@ -72,7 +70,7 @@ function c100307024.setop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x47e0000)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
 		e1:SetValue(LOCATION_REMOVED)
 		c:RegisterEffect(e1)
 	end
