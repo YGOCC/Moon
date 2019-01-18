2af62779ede31036e2efc93ac8db79dad97fc47b
YGOCC <chadoook1990@gmail.com>
2019-01-18 01:37:19 +0100

    Update

--- expansions/script/c249000901.lua
+++ expansions/script/c249000901.lua
@@ -14,14 +14,12 @@ function c249000901.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(89312388,0))
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetCountLimit(1,2490009012)
-	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
-	e2:SetCost(c249000901.coscost)
-	e2:SetOperation(c249000901.cosoperation)
-	c:RegisterEffect(e2)
+	--fusion substitute
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_FUSION_SUBSTITUTE)
+	e3:SetCondition(c249000901.subcon)
+	c:RegisterEffect(e3)
 end
 function c249000901.filter(c)
 	return c:IsCode(249000900) and c:IsAbleToHand()
@@ -38,61 +36,6 @@ function c249000901.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c249000901.filter1(c)
-	return c.material
-end
-function c249000901.coscost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c249000901.filter1,tp,LOCATION_EXTRA,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local g=Duel.SelectMatchingCard(tp,c249000901.filter1,tp,LOCATION_EXTRA,0,1,1,nil)
-	Duel.ConfirmCards(1-tp,g)
-	c249000901.announce_filter={}
-	local k
-	local v
-	local i=3
-	c249000901.announce_filter[1]=g:GetFirst().material[1]
-	c249000901.announce_filter[2]=OPCODE_ISCODE
-	for k, v in pairs(g:GetFirst().material) do
-		if k > 1 then
-			c249000901.announce_filter[i]=v
-			i=i+1
-			c249000901.announce_filter[i]=OPCODE_ISCODE
-			i=i+1
-			c249000901.announce_filter[i]=OPCODE_OR
-			i=i+1
-		end
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CODE)
-	local ac=Duel.AnnounceCardFilter(tp,table.unpack(c249000901.announce_filter))
-	Duel.SetTargetParam(ac)
-	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
-end
-function c249000901.cosoperation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CHANGE_CODE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	e1:SetValue(Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM))
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(249000901,1))
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetCountLimit(1)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	e2:SetLabelObject(e1)
-	e2:SetOperation(c249000901.rstop)
-	c:RegisterEffect(e2)
-end
-function c249000901.rstop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=e:GetLabelObject()
-	e1:Reset()
-	Duel.HintSelection(Group.FromCards(c))
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+function c249000901.subcon(e)
+	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
 end
