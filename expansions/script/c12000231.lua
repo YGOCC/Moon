4e0944b2508f9aa40ef31831e8a2e4a88b057037
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2019-01-29 21:54:16 +0200

    Fixed GM Luna

--- expansions/script/c12000231.lua
+++ expansions/script/c12000231.lua
@@ -72,7 +72,7 @@ function c12000231.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c12000231.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.SendtoHand(tc,tp,REASON_EFFECT)
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
