d36098943df96a2e1bec5bc03abbddbe1deba65c
Lyris12 <lyris89@live.com>
2019-02-01 13:30:14 -0500

    remove obsolete cards by Lyris

--- expansions/script/c240100432.lua
+++
@@ -1,7 +0,0 @@
---created & coded by Lyris, art by G.River of Pixiv
---「S・VINE」アストラル・ドラゴン
-function c240100432.initial_effect(c)
-	c:EnableReviveLimit()
-	aux.AddOrigSpatialType(c)
-	aux.AddSpatialProc(c,nil,8,600,nil,aux.TRUE,aux.TRUE)
-end
