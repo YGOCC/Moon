649be029a15346d9ba9c47611738ac6a2d641dee
Lyris12 <lyris89@live.com>
2019-03-02 14:40:13 -0500

    Add "cyber" by Cyber Assassin

---
+++ expansions/script/c27552503.lua
@@ -0,0 +1,24 @@
+--CARD NAME
+local card = c27552503
+function card.initial_effect(c)
+--draw
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(58098303,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(c27552503.target)
+	e1:SetOperation(c27552503.operation)
+	c:RegisterEffect(e1)
+    end
+	function c27552503.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(3)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
+end
+function c27552503.operation(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
