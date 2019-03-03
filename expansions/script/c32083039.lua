c2ab8bd665c8e3ec11b807f8ebd80106a4ffb2bf
Lyris12 <lyris89@live.com>
2019-03-03 10:32:51 -0500

    Update D.D.Dragons by DD_Kaiser

--- expansions/script/c32083039.lua
+++ expansions/script/c32083039.lua
@@ -1,68 +1,69 @@
 --D.D. Buster Dragon
 function c32083039.initial_effect(c)
-	--fusion material
-	c:EnableReviveLimit()
-	aux.AddFusionProcFunRep2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x7D53),2,63,false)
-	--banish s/t
-	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCondition(c32083039.descon)
-	e2:SetTarget(c32083039.destg)
-	e2:SetOperation(c32083039.desop)
-	c:RegisterEffect(e2)
-	--extra attack
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_REMOVE)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetTarget(c32083039.tgtg)
-	e3:SetOperation(c32083039.tgop)
-	c:RegisterEffect(e3)
+    --fusion material
+    c:EnableReviveLimit()
+    aux.AddFusionProcFunRep2(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x7D53),2,63,false)
+    --banish s/t
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetCondition(c32083039.descon)
+    e2:SetTarget(c32083039.destg)
+    e2:SetOperation(c32083039.desop)
+    c:RegisterEffect(e2)
+    --extra attack
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetDescription(aux.Stringid(32083039,0))
+    e3:SetCountLimit(1)
+    e3:SetTarget(c32083039.tgtg)
+    e3:SetOperation(c32083039.tgop)
+    c:RegisterEffect(e3)
 end
 c32083039.material_setcode=0x7D53
 function c32083039.descon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
 function c32083039.desfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP)
+    return c:IsType(TYPE_SPELL+TYPE_TRAP)
 end
 function c32083039.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and c32083039.desfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c32083039.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	local ct=e:GetHandler():GetMaterialCount()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp, c32083039.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,ct,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+    if chkc then return chkc:IsOnField() and c32083039.desfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(c32083039.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+    local ct=e:GetHandler():GetMaterialCount()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectTarget(tp, c32083039.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,ct,nil)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
 end
 function c32083039.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+    local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
     Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 end
 function c32083039.tgfilter(c)
-	return c:IsSetCard(0x7D53)
+    return c:IsSetCard(0x7D53)
 end
 function c32083039.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c32083039.tgfilter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+    if chk==0 then return Duel.IsExistingMatchingCard(c32083039.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
 end
 function c32083039.tgop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c32083039.tgfilter,tp,LOCATION_DECK,0,1,2,nil)
-	if g:GetCount()==0 then return end
-	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
-	local c=e:GetHandler()
-	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_REMOVED)
-	if ct>0 and c:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_EXTRA_ATTACK)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetValue(ct)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
-	end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,c32083039.tgfilter,tp,LOCATION_DECK,0,1,2,nil)
+    if g:GetCount()==0 then return end
+    Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+    local c=e:GetHandler()
+    local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_REMOVED)
+    if ct>0 and c:IsRelateToEffect(e) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_EXTRA_ATTACK)
+        e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e1:SetValue(ct)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e1)
+    end
 end
