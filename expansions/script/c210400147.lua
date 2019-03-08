a27b2bfd8aa7559744c485787ffed374bd1adf99
Lyris12 <lyris89@live.com>
2019-03-08 16:49:46 -0500

    Fix cards by Lyris
    "Blitzkrieg Dragon - Gold" and "Blitzkrieg Vortex" now display which cards they affect or attempt to affect

--- expansions/script/c210400147.lua
+++ expansions/script/c210400147.lua
@@ -36,7 +36,9 @@ end
 function cid.disop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,cid.dfilter,tp,LOCATION_ONFIELD,0,1,1,aux.ExceptThisCard(e))
-	if g:GetCount()>0 and Duel.Destroy(g,REASON_EFFECT)>0 then
+	if g:GetCount()>0 then
+		Duel.HintSelection(g)
+		if Duel.Destroy(g,REASON_EFFECT)==0 then return end
 		Duel.NegateEffect(ev)
 	end
 end
