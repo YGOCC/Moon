0b9dcc2670fa028392e8d09f7ccd5dd5296a168a
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2019-03-10 17:09:31 +0100

    Fixed "Karasu, the Fallen Nightshade"

--- expansions/script/c62613312.lua
+++ expansions/script/c62613312.lua
@@ -121,7 +121,7 @@ function c62613312.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(math.ceil(e:GetLabel()/2))
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
 		tc:RegisterEffect(e1)
 	end
 end
