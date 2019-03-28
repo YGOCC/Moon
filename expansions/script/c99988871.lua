f4243989f975dfd2ee4723a64920fa8ee9aeb5f1
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2019-03-29 00:51:34 +0100

    Speed Duel Mode as requested by Stormbreaker

---
+++ expansions/script/c99988871.lua
@@ -0,0 +1,340 @@
+--Speed Duel
+--Script by XGlitchy30
+local function getID()
+	local str=string.match(debug.getinfo(2,'S')['source'],"c%d+%.lua")
+	str=string.sub(str,1,string.len(str)-4)
+	local cod=_G[str]
+	local id=tonumber(string.sub(str,2))
+	return id,cod
+end
+local id,cid=getID()
+function cid.initial_effect(c)
+	--Activate	
+	local e0=Effect.CreateEffect(c)	
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e0:SetCode(EVENT_PREDRAW)
+	e0:SetRange(0x5f)
+	e0:SetCountLimit(1,id+EFFECT_COUNT_CODE_DUEL)
+	e0:SetCondition(cid.prevent_conflict)
+	e0:SetOperation(cid.preset)
+	c:RegisterEffect(e0)
+end
+cid.glitched_id = 1
+available_dtypes={
+[1]={TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ+TYPE_LINK,OPCODE_ISTYPE,OPCODE_NOT};
+[2]={0x2889,OPCODE_ISSETCARD,TYPE_PENDULUM,OPCODE_ISTYPE,OPCODE_AND};
+}
+--filters
+function cid.glitched(c)
+	local m=_G["c"..c:GetCode()]
+	if not m then return false end
+	local glitchid=m.glitched_id
+	return glitchid and glitchid>0
+end
+function cid.confirmcard(c,card)
+	return c==card
+end
+function cid.conflict(c,e)
+	local loc=_G["c"..e:GetHandler():GetCode()]
+	local m=_G["c"..c:GetCode()]
+	if not m then return false end
+	local localid=loc.glitched_id
+	local glitchid=m.glitched_id
+	return (glitchid and glitchid<localid) or (glitchid and c:IsControler(1-e:GetHandler():GetControler()) and glitchid<=localid)
+end
+function cid.conflict_info(c,e)
+	local loc=_G["c"..e:GetHandler():GetCode()]
+	local m=_G["c"..c:GetCode()]
+	if not m then return false end
+	local localid=loc.glitched_id
+	local glitchid=m.glitched_id
+	return glitchid and glitchid>=localid
+end
+--Activate
+function cid.prevent_conflict(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(cid.conflict,tp,LOCATION_DECK+LOCATION_HAND,LOCATION_DECK+LOCATION_HAND,1,e:GetHandler(),e)
+		and e:GetHandler():GetFlagEffect(99988870)<=0
+end
+function cid.preset(e,tp,eg,ep,ev,re,r,rp)
+	--Duel Start handfix
+	local drawcount1,drawcount2=0,0
+	local startlimit,startlimit2=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0),Duel.GetFieldGroupCount(1-tp,LOCATION_HAND,0)
+	if startlimit>4 then
+		for i=1,startlimit-4 do
+			local rdm=Duel.GetFieldGroup(tp,LOCATION_HAND,0):GetFirst()
+			Duel.SendtoDeck(rdm,nil,1,REASON_RULE)
+		end
+	end
+	if startlimit2>4 then
+		for i=1,startlimit2-4 do
+			local rdm=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0):GetFirst()
+			Duel.SendtoDeck(rdm,nil,1,REASON_RULE)
+		end
+	end	
+	if Duel.IsExistingMatchingCard(cid.glitched,tp,LOCATION_HAND,LOCATION_HAND,1,nil) then
+		local g=Duel.GetMatchingGroup(cid.glitched,tp,LOCATION_HAND+LOCATION_DECK,LOCATION_HAND+LOCATION_DECK,nil)
+		local hand1=Duel.GetMatchingGroup(cid.glitched,tp,LOCATION_HAND,0,nil)
+		local hand2=Duel.GetMatchingGroup(cid.glitched,tp,0,LOCATION_HAND,nil)
+		drawcount1,drawcount2=hand1:GetCount(),hand2:GetCount()
+		for i in aux.Next(g) do
+			Duel.Remove(i,POS_FACEUP,REASON_RULE)
+			if i:GetControler(tp) then
+				Duel.SendtoDeck(i,nil,1,REASON_RULE)
+			else
+				Duel.SendtoDeck(i,nil,1,REASON_RULE)
+			end
+		end
+	end
+	---------------
+	local excount1,excount2=0,0
+	--remove other Glitched Cards
+	if Duel.IsExistingMatchingCard(cid.conflict_info,tp,LOCATION_DECK,LOCATION_DECK,1,e:GetHandler(),e) then
+		Debug.Message("Found multiple Glitched Cards in the decks with equal or lower ID than Speed Duel Mode")
+		Debug.Message("All other Glitched Cards will be removed")
+		local exclude=Duel.GetMatchingGroup(cid.conflict_info,tp,LOCATION_DECK,LOCATION_DECK,e:GetHandler(),e)
+		for i in aux.Next(exclude) do
+			local p=i:GetControler()
+			Duel.Exile(i,REASON_RULE)
+			if p==tp then
+				excount1=excount1+1
+			else
+				excount2=excount2+1
+			end
+		end
+	end
+	--compensate for opponent	
+	if not Duel.SelectYesNo(1-tp,aux.Stringid(id,2)) then
+		Debug.Message("A player does not want to play with Speed Duel Mode")
+		Debug.Message("The conditions for a Speed Duel have not been met")
+		return
+	end
+	--Speed Duel Mode Activation
+	local cp=Duel.GetMatchingGroup(Card.IsCode,tp,0,LOCATION_DECK,e:GetHandler(),id)
+	--fix for opponent's multiples
+	if cp:GetCount()>0 then
+		local remainder=cp:GetCount()
+		Debug.Message("Found multiple copies of Speed Duel Mode in your opponent's Deck. Wait while we remove the unnecessary ones")
+		for tm=1,remainder do
+			local exc=cp:GetFirst()
+			Duel.Exile(exc,REASON_RULE)
+			excount2=excount2+1
+		end
+	end
+	-----------
+	Duel.Exile(e:GetHandler(),REASON_RULE)
+	excount1=excount1+1
+	--check valid deck size
+	local checksz,checksz2=Duel.GetFieldGroupCount(tp,LOCATION_DECK+LOCATION_HAND,0),Duel.GetFieldGroupCount(1-tp,LOCATION_DECK+LOCATION_HAND,0)
+	if checksz>30 or checksz2>30 then
+		Debug.Message("Found a deck that contains more than 30 cards. The Duel will be stopped, please adjust your Deck in Deck Building")
+		Duel.Win(tp,0x1)
+	end
+	--add missing cards
+	if excount1>0 then
+		local cond=false
+		local check1=Duel.GetFieldGroupCount(tp,LOCATION_DECK+LOCATION_HAND,0)
+		--decide if mandatory or optional
+		if check1<20 then
+			cond=true
+		elseif check1<30 then
+			if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+				cond=true
+			else
+				cond=false
+			end
+		else
+			cond=false
+		end
+		---------------
+		if cond then
+			local prep={}
+			local slots
+			local minm=1
+			local fixcount=excount1
+			if check1<20 then
+				Debug.Message("Your deck size is less than 20. Please add the minimum number of cards")
+				minm=20-check1
+			end
+			if check1+fixcount>30 then
+				fixcount=30-check1
+			end
+			if fixcount>0 then
+				for maxm=minm,fixcount do
+					table.insert(prep,maxm)
+				end
+				Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+				local ac=Duel.AnnounceNumber(tp,table.unpack(prep))
+				while ac>0 do
+					local dtype=available_dtypes[1]
+					local cd=0
+					if dtype then
+						cd=Duel.AnnounceCardFilter(tp,table.unpack(dtype))
+					else
+						cd=Duel.AnnounceCard(tp)
+					end
+					--choose correct number
+					if ac>2 then
+						slots = {1,2,3}
+					elseif ac>1 then
+						slots = {1,2}
+					elseif ac==1 then
+						slots = {1}
+					end
+					-----------
+					local ctcard=Duel.AnnounceNumber(tp,table.unpack(slots))
+					for cardnum=1,ctcard do
+						local card=Duel.CreateToken(tp,cd)
+						Duel.SendtoDeck(card,tp,0,REASON_RULE)
+						if ac>0 then
+							ac=ac-1
+						end
+					end
+				end
+			end
+		end
+	end
+	if excount2>0 then
+		local cond=false
+		local check1=Duel.GetFieldGroupCount(1-tp,LOCATION_DECK+LOCATION_HAND,0)
+		--decide if mandatory or optional
+		if check1<20 then
+			cond=true
+		elseif check1<30 then
+			if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+				cond=true
+			else
+				cond=false
+			end
+		else
+			cond=false
+		end
+		---------------
+		if cond then
+			local minm=1
+			local fixcount=excount2
+			local prep={}
+			local slots
+			if check1<20 then
+				Debug.Message("Your deck size is less than 20. Please add the minimum number of cards")
+				minm=20-check1
+			end
+			if check1+fixcount>30 then
+				fixcount=30-check1
+			end
+			for maxm=minm,fixcount do
+				table.insert(prep,maxm)
+			end
+			if fixcount>0 then
+				Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(id,3))
+				local ac=Duel.AnnounceNumber(1-tp,table.unpack(prep))
+				while ac>0 do
+					local dtype=available_dtypes[1]
+					local cd=0
+					if dtype then
+						cd=Duel.AnnounceCardFilter(1-tp,table.unpack(dtype))
+					else
+						cd=Duel.AnnounceCard(1-tp)
+					end
+					--choose correct number
+					if ac>2 then
+						slots = {1,2,3}
+					elseif ac>1 then
+						slots = {1,2}
+					elseif ac==1 then
+						slots = {1}
+					end
+					-----------
+					local ctcard=Duel.AnnounceNumber(1-tp,table.unpack(slots))
+					for cardnum=1,ctcard do
+						local card=Duel.CreateToken(1-tp,cd)
+						Duel.SendtoDeck(card,nil,0,REASON_RULE)
+						if ac>0 then
+							ac=ac-1
+						end
+					end
+				end
+			end
+		end
+	end
+	Duel.ShuffleDeck(tp)
+	Duel.ShuffleDeck(1-tp)
+	Duel.Draw(tp,drawcount1,REASON_RULE)
+	Duel.Draw(1-tp,drawcount2,REASON_RULE)
+	--adjust LPs
+	for lpp=0,1 do
+		if Duel.GetLP(lpp)~=4000 then
+			Duel.SetLP(lpp,4000)
+		end
+	end
+	--adjust extra deck
+	local extrag=Group.CreateGroup()
+	for extrap=0,1 do
+		local extra=Duel.GetFieldGroup(extrap,LOCATION_EXTRA,0)
+		if #extra>5 then
+			local extrile=extra:Select(extrap,#extra-5,#extra-5,nil)
+			extrag:Merge(extrile)
+		end
+	end
+	if #extrag>0 then
+		Duel.Exile(extrag,REASON_RULE)
+	end
+	--modify field
+	local df1=Effect.CreateEffect(e:GetHandler())
+	df1:SetType(EFFECT_TYPE_FIELD)
+	df1:SetCode(EFFECT_DISABLE_FIELD)
+	df1:SetOperation(function (e,tp)
+						return bit.lshift(0x1,0)
+					end)
+	Duel.RegisterEffect(df1,tp)
+	local df2=df1:Clone()
+	df2:SetOperation(function (e,tp)
+						return bit.lshift(0x1,4)
+					end)
+	Duel.RegisterEffect(df2,tp)
+	local df3=df1:Clone()
+	Duel.RegisterEffect(df3,1-tp)
+	local df4=df2:Clone()
+	Duel.RegisterEffect(df4,1-tp)
+	local df5=df1:Clone()
+	df5:SetOperation(function (e,tp)
+						return bit.lshift(0x100,0)
+					end)
+	Duel.RegisterEffect(df5,tp)
+	local df6=df1:Clone()
+	df6:SetOperation(function (e,tp)
+						return bit.lshift(0x100,4)
+					end)
+	Duel.RegisterEffect(df6,tp)
+	local df7=df5:Clone()
+	Duel.RegisterEffect(df7,1-tp)
+	local df8=df6:Clone()
+	Duel.RegisterEffect(df8,1-tp)
+	--skip phase
+	local sk=Effect.CreateEffect(e:GetHandler())
+	sk:SetType(EFFECT_TYPE_FIELD)
+	sk:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	sk:SetTargetRange(1,1)
+	sk:SetCode(EFFECT_SKIP_M2)
+	Duel.RegisterEffect(sk,tp)
+	--adjust hand limit
+	local hl=Effect.CreateEffect(e:GetHandler())
+	hl:SetType(EFFECT_TYPE_FIELD)
+	hl:SetCode(EFFECT_HAND_LIMIT)
+	hl:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	hl:SetTargetRange(1,1)
+	hl:SetValue(6)
+	Duel.RegisterEffect(hl,tp)
+	--enable skills
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) and Duel.SelectYesNo(1-tp,aux.Stringid(id,0)) then
+		for pp=0,1 do
+			local dtype=available_dtypes[2]
+			local cd=0
+			cd=Duel.AnnounceCardFilter(pp,table.unpack(dtype))
+			local card=Duel.CreateToken(pp,cd)
+			Duel.Remove(card,POS_FACEUP,REASON_RULE)
+			Duel.SendtoExtraP(card,pp,REASON_RULE)
+			card:RegisterFlagEffect(id,RESET_EVENT+EVENT_CUSTOM+id,EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SET_AVAILABLE,1)
+		end
+	end
+end
