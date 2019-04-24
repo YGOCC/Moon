c36000c1959b145be911bcd3c320dba1dfb2f5a7
Lyris12 <lyris89@live.com>
2019-04-24 15:23:03 -0400

    Update cards by Lyris
    "Blitzkrieg Blast" Skill Card: Effect is now worded like a Continuous effect
    Buff "Blitzkrieg" Fusion and LINK-4 monsters: Self destruction effect has been greatly upgraded

--- expansions/script/c210400125.lua
+++ expansions/script/c210400125.lua
@@ -22,16 +22,6 @@ function cid.initial_effect(c)
 	e2:SetTarget(cid.tg)
 	e2:SetOperation(cid.op)
 	c:RegisterEffect(e2)
-	local e0=Effect.CreateEffect(c)
-	e0:SetCategory(CATEGORY_DESTROY)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e0:SetProperty(EFFECT_FLAG_DELAY)
-	e0:SetRange(LOCATION_MZONE)
-	e0:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e0:SetCondition(cid.descon)
-	e0:SetTarget(cid.destg)
-	e0:SetOperation(cid.desop)
-	c:RegisterEffect(e0)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -44,6 +34,56 @@ function cid.initial_effect(c)
 	e5:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
 	e5:SetValue(aux.imval1)
 	c:RegisterEffect(e5)
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e0:SetType(EFFECT_TYPE_QUICK_O)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_ATTACK)
+	e0:SetCondition(cid.descon)
+	e0:SetTarget(cid.destg)
+	e0:SetOperation(cid.desop)
+	c:RegisterEffect(e0)
+end
+function cid.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP() or (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE)
+end
+function cid.desfilter(c,e,tp)
+	return c:IsLevelBelow(5) and c:IsSetCard(0x7c4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cid.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDestructable() and Duel.GetMZoneCount(tp,c)>0
+		and Duel.IsExistingMatchingCard(cid.desfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function cid.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.Destroy(c,REASON_EFFECT)~=0 then
+		local tc=Duel.GetAttacker()
+		if tc and tc:IsControler(1-tp) then Duel.NegateAttack()
+		else
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			e1:SetCountLimit(1)
+			e1:SetCondition(function(e,tp) return Duel.GetTurnPlayer()~=tp end)
+			e1:SetOperation(cid.disop)
+			Duel.RegisterEffect(e1,tp)
+		end
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cid.desfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+function cid.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.NegateAttack()
 end
 function cid.tgfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x7c4) and c:IsLevelBelow(5)
@@ -54,22 +94,6 @@ end
 function cid.etarget(e,re)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
 	return g and g:IsContains(e:GetHandler())
-end
-function cid.descon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return Duel.GetTurnPlayer()~=tp and c:IsFaceup() and Duel.GetAttackTarget()==c
-end
-function cid.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
-end
-function cid.desop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	Duel.NegateAttack()
-	if c:IsRelateToEffect(e) then
-		Duel.BreakEffect()
-		Duel.Destroy(c,REASON_EFFECT)
-	end
 end
 function cid.cfilter(c,tp)
 	return (c:IsPreviousLocation(LOCATION_MZONE) or c:IsType(TYPE_MONSTER)) and (c:IsPreviousPosition(POS_FACEUP) or c:GetPreviousControler()==tp) and c:IsSetCard(0x7c4) and c:IsType(TYPE_MONSTER)
