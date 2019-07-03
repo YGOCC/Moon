8cd18678f58d995a164a37178cd3b931e6485476
Clara <lyris89@live.com>
2019-07-03 08:38:01 -0400

    Fix "Blood Arts - The Isolated Town"
    Was not applying the ATK boost upon the user taking damage

--- expansions/script/c231862014.lua
+++ expansions/script/c231862014.lua
@@ -42,16 +42,19 @@ function cid.initial_effect(c)
 	e5:SetOperation(cid.actop)
 	c:RegisterEffect(e5)
 end
+function cid.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x52f)
+end
 function cid.op(e,tp,eg,ep,ev,re,r,rp)
 	if ep~=tp then return end
-	Duel.Hint(HINT_CARD,0,id)
-	local g=Duel.GetMatchingGroup(aux.AND(aux.FilterBoolFunction(Card.IsFaceup),aux.FilterBoolFunction(Card.IsSetCard,0x52f)),tp,LOCATION_MZONE,0,nil)
+	local g=Duel.GetMatchingGroup(cid.filter,tp,LOCATION_MZONE,0,nil)
+	if #g>0 then Duel.Hint(HINT_CARD,0,id) end
 	for tc in aux.Next(g) do
 		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_UPDATE_ATTACK)
-		e2:SetRange(LOCATION_FZONE)
-		e2:SetTargetRange(LOCATION_MZONE,0)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e2:SetValue(100)
 		tc:RegisterEffect(e2)
 		local e3=e2:Clone()
