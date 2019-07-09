a5e709ff31fe01dc1e612ebdc52d40c1f08a2027
YGOCC <chadoook1990@gmail.com>
2019-07-09 14:46:28 +0100

    ALL :star2:

---
+++ expansions/script/c160006868.lua
@@ -0,0 +1,7 @@
+--ペイントレディ ＥX クレセント・ゴーギア 
+function c160006868.initial_effect(c)
+	c:EnableReviveLimit()
+	  --E summon
+	aux.AddOrigEvoluteType(c)
+   aux.AddEvoluteProc(c,nil,7,aux.TRUE,aux.TRUE,2,99)
+end
