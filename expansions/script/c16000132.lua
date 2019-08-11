1e24c7d23ec936b26a4dd253d8d3e4fa9089842b
YGOCC <chadoook1990@gmail.com>
2019-08-11 15:06:04 +0100

    More cute horsey than moon

---
+++ expansions/script/c16000132.lua
@@ -0,0 +1,90 @@
+--Seedhorse of Fiber VINE
+local cid,id=GetID()
+function cid.initial_effect(c)
+	 --link summon
+	aux.AddLinkProcedure(c,nil,2,2,cid.lcheck)
+	c:EnableReviveLimit()
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(cid.hspcon)
+	e1:SetTarget(cid.hsptg)
+	e1:SetOperation(cid.hspop)
+	 c:RegisterEffect(e1)
+		   --special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(2,id+100)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(cid.cost)
+	e2:SetTarget(cid.target)
+	e2:SetOperation(cid.operation)
+	c:RegisterEffect(e2)
+end
+function cid.lcheck(g,lc)
+	return g:IsExists(Card.IsSetCard,1,nil,0x85a)
+end
+function cid.hspcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function cid.hspfilter(c,e,tp)
+	return c:IsSetCard(0x185a) and  c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cid.hsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(cid.hspfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function cid.hspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,cid.hspfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+	Duel.SpecialSummonComplete()
+end
+function cid.filter(c,tp)
+	return c:IsSetCard(0x185a) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+		and Duel.IsExistingMatchingCard(cid.filter2,tp,LOCATION_DECK,0,1,nil,c)
+end
+function cid.filter2(c,mc)
+	return c:IsSetCard(0x85a) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand() 
+end
+function cid.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if Duel.IsPlayerAffectedByEffect(tp,EFFECT_DISCARD_COST_CHANGE) then return true end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,2,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,2,2,REASON_COST+REASON_DISCARD)
+end
+function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_DECK,0,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function cid.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,cid.filter,tp,LOCATION_DECK,0,1,1,nil,tp)
+	if g:GetCount()>0 then
+		local mg=Duel.GetMatchingGroup(cid.filter2,tp,LOCATION_DECK,0,nil,g:GetFirst())
+		if mg:GetCount()>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=mg:Select(tp,1,1,nil)
+			g:Merge(sg)
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
