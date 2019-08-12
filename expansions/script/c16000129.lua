6f8cff9575989ddb5600d54b4db93424cb0a8692
YGOCC <chadoook1990@gmail.com>
2019-08-12 01:52:10 +0100

    Darned bugs

--- expansions/script/c16000129.lua
+++ expansions/script/c16000129.lua
@@ -22,12 +22,13 @@ end
 
 -- SpecialSummon from hand
 function c16000129.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x185a) and not c:IsCode(16000129)
+	return c:IsFacedown() or not c:IsSetCard(0x185a) or  c:IsCode(16000129)
 end
 function c16000129.spcon(e,c)
- if c==nil then return true end
-	  return  Duel.IsExistingMatchingCard(c16000129.cfilter,tp,LOCATION_MZONE,0,1,nil) or  Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0,nil)==0
-		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return   Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and (Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 or not Duel.IsExistingMatchingCard(c16000129.cfilter,tp,LOCATION_MZONE,0,1,nil))
 end
 
 function c16000129.filter(c,e,sp)
