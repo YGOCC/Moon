4f90a3be1feb88b40817a5983318bc418c699de7
Clara <lyris89@live.com>
2019-08-20 05:15:36 -0400

    Update Chroma procedures by Lyris

--- expansions/script/proc_chroma.lua
+++ expansions/script/proc_chroma.lua
@@ -1,5 +1,4 @@
-EFFECT_CANNOT_BE_CHROMA_MATERIAL	=779
-EFFECT_MUST_BE_CHROMA_MATERIAL		=780
+EFFECT_CANNOT_HAVE_CHROMA_MATERIAL	=779
 TYPE_CHROMA							=0x1000000000000
 TYPE_CUSTOM							=TYPE_CUSTOM|TYPE_CHROMA
 CTYPE_CHROMA						=0x10000
@@ -54,11 +53,11 @@ Card.GetReason=function(c)
 end
 
 --Custom Functions
-function Card.IsCanBeChromaMaterial(c,cc)
-	if c:IsStatus(STATUS_NO_LEVEL) or c:IsFacedown() then return false end
-	local tef2={c:IsHasEffect(EFFECT_CANNOT_BE_CHROMA_MATERIAL)}
+function Card.IsCanBeChromaMaterial(c,mc)
+	if mc:IsStatus(STATUS_NO_LEVEL) or mc:IsFacedown() then return false end
+	local tef2={mc:IsHasEffect(EFFECT_CANNOT_HAVE_CHROMA_MATERIAL)}
 	for _,te2 in ipairs(tef2) do
-		if te2:GetValue()(te2,cc) then return false end
+		if te2:GetValue()(te2,c) then return false end
 	end
 	return true
 end
@@ -69,35 +68,22 @@ function Auxiliary.AddOrigChromaType(c,isritual)
 	Auxiliary.Chromas[c]=function() return isritual end
 end
 function Auxiliary.AddChromaProc(c,f)
-	if not CHROMA_CHECKLIST then
-		CHROMA_CHECKLIST=0
-		local ge1=Effect.GlobalEffect()
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_PHASE_START+PHASE_DRAW)
-		ge1:SetOperation(Auxiliary.ChromaReset)
-		Duel.RegisterEffect(ge1,0)
-	end
 	local ge2=Effect.CreateEffect(c)
 	ge2:SetType(EFFECT_TYPE_FIELD)
 	ge2:SetCode(EFFECT_SPSUMMON_PROC_G)
-	ge2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	ge2:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	ge2:SetRange(LOCATION_HAND)
 	ge2:SetCondition(Auxiliary.ChromaCondition(f))
 	ge2:SetOperation(Auxiliary.ChromaOperation(f))
 	ge2:SetValue(14)
 	c:RegisterEffect(ge2)
 end
-function Auxiliary.ChromaReset(e,tp,eg,ep,ev,re,r,rp)
-	CHROMA_CHECKLIST=0
-end
 function Auxiliary.CMaterialFilter(c,e,tp,g,cc,f)
 	local sg,mg=Group.FromCards(cc),Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-	return c:IsLevelAbove(1) and not c:IsType(TYPE_CHROMA) and (not f or f(c))
+	return not c:IsType(TYPE_CHROMA) and (not f or f(c))
 		and mg:IsExists(Auxiliary.CConditionFilter,1,sg,e,tp,sg,mg,c)
 end
 function Auxiliary.CConditionFilter(c,e,tp,sg,mg,mat)
-	if not (c:IsType(TYPE_CHROMA) and (c:IsAttribute(mat:GetAttribute()) or c:IsRace(mat:GetRace()))
-		and c:IsCanBeSpecialSummoned(e,14,tp,true,true) and not c:IsForbidden()) then return false end
 	sg:AddCard(c)
 	local res=Auxiliary.ChromaGoal(sg,e,tp,mat)
 		or mg:IsExists(Auxiliary.CConditionFilter,1,sg,e,tp,sg,mg,mat)
@@ -106,9 +92,13 @@ function Auxiliary.CConditionFilter(c,e,tp,sg,mg,mat)
 end
 function Auxiliary.ChromaGoal(sg,e,tp,mat)
 	local ct=#sg
-	return ct>1 and Duel.GetMZoneCount(tp,mat)>=ct
-		and mat:GetLevel()==sg:GetSum(Card.GetLevel)
-	and sg:IsExists(Card.IsCanBeChromaMaterial,ct,nil,mat)
+	return ct>1 and Duel.GetUsableMZoneCount(tp)>ct-1
+		and mat:GetLevel()==sg:GetSum(function(c) local lv,rk,st,sb,dn,fr,cl=c:GetLevel(),c:GetRank(),c:GetStage(),c:GetStability(),c:GetDimensionNo(),c:GetFuture(),c:GetCell() return (lv>0 and lv) or (rk>0 and rk) or (st>0 and st) or (sb>0 and sb) or (dn>0 and dn) or (fr>0 and fr) or (cl>0 and cl) or Duel.ReadCard(c,CARDDATA_LEVEL) end)
+		and sg:IsExists(Auxiliary.ChromaCheck,ct,nil,mat,e,tp)
+end
+function Auxiliary.ChromaCheck(c,mat,e,tp)
+	return c:IsType(TYPE_CHROMA) and (c:IsAttribute(mat:GetAttribute()) or c:IsRace(mat:GetRace()))
+		and c:IsCanBeSpecialSummoned(e,14,tp,true,true) and not c:IsForbidden() and c:IsCanBeChromaMaterial(mat)
 end
 function Auxiliary.ChromaCondition(f)
 	return	function(e,c,og)
@@ -138,17 +128,16 @@ function Auxiliary.ChromaOperation(f)
 				end
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 				local tc=tg:Select(tp,Auxiliary.CMaterialFilter,1,1,nil,e,tp,tg,c,f):GetFirst()
-				local ft=Duel.GetMZoneCount(tp,tc)
+				local ft=Duel.GetUsableMZoneCount(tp)
 				if Duel.IsPlayerAffectedByEffect(tp,59822133) then ft=1 end
-				local sg=Group.FromCards(c)
+				local smg=Group.FromCards(c)
 				local mg=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
-				mg=mg:Filter(Auxiliary.CConditionFilter,sg,e,tp,sg,mg,tc)
+				Duel.SetSelectedCard(smg)
 				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 				Auxiliary.GCheckAdditional=Auxiliary.ChromaOperationCheck(ft)
 				local g=mg:SelectSubGroup(tp,Auxiliary.ChromaGoal,true,2,math.min(#mg,ft),e,tp,tc)
 				Auxiliary.GCheckAdditional=nil
 				if not g then return end
-				CHROMA_CHECKLIST=CHROMA_CHECKLIST|(0x1<<tp)
 				Duel.HintSelection(Group.FromCards(tc))
 				Duel.SendtoGrave(tc,REASON_MATERIAL+REASON_CHROMA)
 				tc:SetMaterial(g)
