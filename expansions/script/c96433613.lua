c03838cb845aa0d76d54201ec795625692e0af24
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2019-08-25 15:25:24 +0200

    Meteorite impact + updated cdb entried for BTED cards

---
+++ expansions/script/c96433613.lua
@@ -0,0 +1,52 @@
+--	M   M	EEEEE	TTTTT	EEEEE	 OOO 	RRRR 	IIIII	TTTTT	EEEEE
+--	MM MM	EE		  T		EE		O   O	RR  R	 III 	  T  	EE
+--	M M M	EEEEE	  T		EEEEE	O   O	RRRR 	 III	  T		EEEEE
+--	M   M	EE		  T		EE		O   O	RR  R	 III	  T		EE
+--	M   M	EEEEE     T		EEEEE	 OOO 	RR  R	IIIII	  T		EEEEE
+
+--Scripted by: XGlitchy30
+
+local function getID()
+	local str=string.match(debug.getinfo(2,'S')['source'],"c%d+%.lua")
+	str=string.sub(str,1,string.len(str)-4)
+	local cod=_G[str]
+	local id=tonumber(string.sub(str,2))
+	return id,cod
+end
+local id,cid=getID()
+function cid.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(cid.target)
+	e1:SetOperation(cid.activate)
+	c:RegisterEffect(e1)
+end
+--Activate
+--Filters--
+function cid.checkfilter(c,e)
+	return c:IsDestructable(e)
+end
+-----------
+function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,0,LOCATION_ONFIELD,1,e:GetHandler(),TYPE_SPELL+TYPE_TRAP) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_SZONE)
+end
+function cid.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsType,tp,0,LOCATION_SZONE,e:GetHandler(),TYPE_SPELL+TYPE_TRAP)
+	local chkg=g:Filter(cid.checkfilter,nil,e)
+	while #chkg>0 do
+		Duel.SetLP(tp,Duel.GetLP(tp)-2000)
+		if Duel.GetLP(tp)<=0 then return end
+		Duel.BreakEffect()
+		if #g<=0 then return end
+		local sg=g:Select(tp,1,1,nil)
+		if #sg>0 then
+			Duel.Destroy(sg,REASON_EFFECT)
+			g=Duel.GetMatchingGroup(Card.IsType,tp,0,LOCATION_SZONE,e:GetHandler(),TYPE_SPELL+TYPE_TRAP)
+			chkg=g:Filter(cid.checkfilter,nil,e)
+		end
+	end
+end
