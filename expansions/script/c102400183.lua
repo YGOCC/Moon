c2270d039e49741cd286f6e4d26e71b7d263f500
Clara <lyris89@live.com>
2019-09-01 10:55:36 -0400

    Preview 3: Rework "Newtrix" archetype by Lyris

--- expansions/script/c102400183.lua
+++ expansions/script/c102400183.lua
@@ -21,13 +21,14 @@ function cid.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0xd10) and c:IsAbleToHand()
 end
 function cid.activate(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if not e:GetHandler():IsRelateToEffect(e) or Duel.GetFlagEffect(tp,id)~=0 then return end
 	local g=Duel.GetMatchingGroup(cid.filter,tp,LOCATION_REMOVED,0,nil)
 	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 	end
 end
 function cid.cfilter(c)
