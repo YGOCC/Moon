3143471fe0894cd1bf651067dd2e5918a085314c
Clara <lyris89@live.com>
2019-09-03 13:32:25 -0400

    Fix "Star Bearer" cards
    Neither "Veil" effect was HOpT
    "Lightsworn" 2 effect returned cards to the top of the Deck

--- expansions/script/c888700.lua
+++ expansions/script/c888700.lua
@@ -2,77 +2,77 @@
 local m=888700
 local cm=_G["c"..m]
 function cm.initial_effect(c)
-    local e2=Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_BE_MATERIAL)
-    e2:SetProperty(EFFECT_FLAG_DELAY)
-    e2:SetCountLimit(1,8887002)
-    e2:SetCondition(cm.tgcon)
-    e2:SetTarget(cm.target1)
-    e2:SetOperation(cm.operation1)
-    c:RegisterEffect(e2)
-    local e4=Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(23064604,2))
-    e4:SetCategory(CATEGORY_TOHAND)
-    e4:SetType(EFFECT_TYPE_QUICK_O)
-    e4:SetCode(EVENT_FREE_CHAIN)
-    e4:SetRange(LOCATION_GRAVE)
-    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e4:SetCode(EVENT_FREE_CHAIN)
-    e4:SetCountLimit(1,8887004)
-    e4:SetCondition(cm.thcon)
-    e4:SetCost(cm.thcost)
-    e4:SetTarget(cm.thtg)
-    e4:SetOperation(cm.thop)
-    c:RegisterEffect(e4)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,8887002)
+	e2:SetCondition(cm.tgcon)
+	e2:SetTarget(cm.target1)
+	e2:SetOperation(cm.operation1)
+	c:RegisterEffect(e2)
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(23064604,2))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetCountLimit(1,8887004)
+	e4:SetCondition(cm.thcon)
+	e4:SetCost(cm.thcost)
+	e4:SetTarget(cm.thtg)
+	e4:SetOperation(cm.thop)
+	c:RegisterEffect(e4)
 end
 function cm.thcon(e,tp,eg,ep,ev,re,r,rp)
-    local ph=Duel.GetCurrentPhase()
-    return Duel.GetTurnPlayer()~=tp 
+	local ph=Duel.GetCurrentPhase()
+	return Duel.GetTurnPlayer()~=tp 
 end
 function cm.tgcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local rc=c:GetReasonCard()
-    return c:IsLocation(LOCATION_GRAVE) and rc:IsSetCard(0xff1) and r&REASON_FUSION~=0
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	return c:IsLocation(LOCATION_GRAVE) and rc:IsSetCard(0xff1) and r&REASON_FUSION~=0
 end
 function cm.filter1(c)
-    return c:IsSetCard(0xff1) and c:IsAbleToDeck()
+	return c:IsSetCard(0xff1) and c:IsAbleToDeck()
 end
 function cm.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and cm.filter1(chkc) end
-    if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsExistingTarget(cm.filter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-    local g=Duel.SelectTarget(tp,cm.filter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and cm.filter1(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsExistingTarget(cm.filter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,cm.filter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,3,3,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,g:GetCount(),0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function cm.operation1(e,tp,eg,ep,ev,re,r,rp)
-    local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-    if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=3 then return end
-    Duel.SendtoDeck(tg,nil,0,REASON_EFFECT)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	if not tg or tg:FilterCount(Card.IsRelateToEffect,nil,e)~=3 then return end
+	Duel.SendtoDeck(tg,nil,2,REASON_EFFECT)
 end
 function cm.cfilter(c)
-    return c:IsSetCard(0xff1) and c:IsDiscardable()
+	return c:IsSetCard(0xff1) and c:IsDiscardable()
 end
 function cm.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(cm.cfilter,tp,LOCATION_HAND,0,1,nil) end
-    Duel.DiscardHand(tp,cm.cfilter,1,1,REASON_COST+REASON_DISCARD)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.cfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,cm.cfilter,1,1,REASON_COST+REASON_DISCARD)
 end
 function cm.thfilter(c)
-    return c:IsSetCard(0xff1)
+	return c:IsSetCard(0xff1)
 end
 function cm.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cm.thfilter(chkc) end
-    if chk==0 then return Duel.IsExistingTarget(cm.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectTarget(tp,cm.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and cm.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(cm.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,cm.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function cm.thop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc:IsRelateToEffect(e) then
-        Duel.SendtoHand(tc,nil,REASON_EFFECT)
-    end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
 end
