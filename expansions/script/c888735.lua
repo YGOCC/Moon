adbb8d158a70fcc1dd973565774927548bffbccb
Clara <lyris89@live.com>
2019-09-03 13:22:48 -0400

    Fix "World Born By Star Bearers"
    Could not banish cards which could not be targeted

--- expansions/script/c888735.lua
+++ expansions/script/c888735.lua
@@ -2,82 +2,82 @@
 local m=888735
 local cm=_G["c"..m]
 function cm.initial_effect(c)
-        --Activate
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCountLimit(1,m+EFFECT_COUNT_CODE_OATH)
-    e1:SetOperation(cm.activate)
-    c:RegisterEffect(e1)
-       --atk&def
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_UPDATE_ATTACK)
-    e2:SetRange(LOCATION_FZONE)
-    e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-    e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xff1))
-    e2:SetValue(300)
-    c:RegisterEffect(e2)
-    local e3=e2:Clone()
-    e3:SetCode(EFFECT_UPDATE_DEFENSE)
-    c:RegisterEffect(e3)
-    local e4=Effect.CreateEffect(c)
-    e4:SetCategory(CATEGORY_REMOVE)
-    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e4:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e4:SetProperty(EFFECT_FLAG_DELAY)
-    e4:SetRange(LOCATION_FZONE)
-    e4:SetCondition(cm.tg)
-    e4:SetOperation(cm.regop)
-    c:RegisterEffect(e4)
+		--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,m+EFFECT_COUNT_CODE_OATH)
+	e1:SetOperation(cm.activate)
+	c:RegisterEffect(e1)
+	   --atk&def
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0xff1))
+	e2:SetValue(300)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCondition(cm.tg)
+	e4:SetOperation(cm.regop)
+	c:RegisterEffect(e4)
 end
 
 function cm.cfilter(c,e,tp)
-    return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsSummonType(SUMMON_TYPE_FUSION) and c:IsSetCard(0xff1)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:IsSummonType(SUMMON_TYPE_FUSION) and c:IsSetCard(0xff1)
 end
 
 function cm.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    return eg:IsExists(cm.cfilter,1,nil,e,tp)
+	return eg:IsExists(cm.cfilter,1,nil,e,tp)
 end
 
 function cm.regop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(32904923,1))
-    e1:SetCategory(CATEGORY_REMOVE)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_PHASE+PHASE_END)
-    e1:SetCountLimit(1,m)
-    e1:SetRange(LOCATION_DECK+LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_HAND+LOCATION_OVERLAY+LOCATION_REMOVED)
-    e1:SetTarget(cm.btg)
-    e1:SetOperation(cm.bop)
-    e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-    c:RegisterEffect(e1)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(32904923,1))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1,m)
+	e1:SetRange(LOCATION_DECK+LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_HAND+LOCATION_OVERLAY+LOCATION_REMOVED)
+	e1:SetTarget(cm.btg)
+	e1:SetOperation(cm.bop)
+	e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
 end
 
 function cm.thfilter(c)
-    return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xff1) and c:IsAbleToHand()
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0xff1) and c:IsAbleToHand()
 end
 function cm.activate(e,tp,eg,ep,ev,re,r,rp)
-    if not e:GetHandler():IsRelateToEffect(e) then return end
-    local g=Duel.GetMatchingGroup(cm.thfilter,tp,LOCATION_DECK,0,nil)
-    if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(5697558,0)) then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local sg=g:Select(tp,1,1,nil)
-        Duel.SendtoHand(sg,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,sg)
-    end
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(cm.thfilter,tp,LOCATION_DECK,0,nil)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(5697558,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
 end
-function cm.btg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsAbleToRemove() end
-    if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-    local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+function cm.btg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
 end
 function cm.bop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc:IsRelateToEffect(e) then Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
-    end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
 end
