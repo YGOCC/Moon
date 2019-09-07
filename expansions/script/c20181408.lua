948a1d14c6159c5235b775c61a005a528e830964
XGlitchy30 <34371410+XGlitchy30@users.noreply.github.com>
2019-09-08 00:28:40 +0200

    twintyranny fix

--- expansions/script/c20181408.lua
+++ expansions/script/c20181408.lua
@@ -2,7 +2,7 @@
 local cid,id=GetID()
 function cid.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,aux.FilterBoolFunction(cid.matfilter),2,2,cid.lcheck)
+	aux.AddLinkProcedure(c,cid.matfilter,2,2,cid.lcheck)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
