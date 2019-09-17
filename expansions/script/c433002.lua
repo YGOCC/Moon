19ddf16466d26c46896c06ac001db5b13e232b65
Clara <lyris89@live.com>
2019-09-17 12:32:05 -0400

    Update cards from community packs
    Add "Sepialife" Archetype by Alpha
    Add 6 Generic Attribute Based Time Leap Monsters for Level 4s by Swag

---
+++ expansions/script/c433002.lua
@@ -0,0 +1,132 @@
+--Relentless Domination Commander
+--Created and Scripted by Swaggy
+local function getID()
+	local str=string.match(debug.getinfo(2,'S')['source'],"c%d+%.lua")
+	str=string.sub(str,1,string.len(str)-4)
+	local cod=_G[str]
+	local id=tonumber(string.sub(str,2))
+	return id,cod
+end
+local id,cid=getID()
+function cid.initial_effect(c)
+	--time leap procedure
+	aux.AddOrigTimeleapType(c,false)
+	aux.AddTimeleapProc(c,5,cid.sumcon,cid.tlfilter,nil)
+	c:EnableReviveLimit() 
+	--Smack dat ass TWICE
+		local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+	--Slap my Nuts and call me buffed
+		local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(433002,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(cid.buffcon)
+	e1:SetOperation(cid.buffop)
+	c:RegisterEffect(e1)
+	--Is this a Peach Beach card?
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS+EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1)
+	e2:SetCondition(cid.lpcon)
+	e2:SetTarget(cid.lptg)
+	e2:SetOperation(cid.lpop)
+	c:RegisterEffect(e2)
+	--:clap: :clap: REVIVE REVIEW :clap: :clap:
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+	e3:SetCondition(cid.revcon)
+	e3:SetTarget(cid.revtg)
+	e3:SetOperation(cid.revop)
+	c:RegisterEffect(e3)
+	end
+	function cid.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>=3
+	end
+	function cid.tlfilter(c,e,mg)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:GetLevel()==e:GetHandler():GetFuture()-1
+end
+	function cid.buffcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_TIMELEAP)
+end
+	function cid.buffop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_MZONE,0,ATTRIBUTE_WATER)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		tc:RegisterEffect(e2)
+		tc=g:GetNext()
+	end
+end
+function cid.lpfilter(c,tp)
+	return c:GetSummonPlayer()==tp and c:IsAttribute(ATTRIBUTE_WATER)
+end
+function cid.lpcon(e,tp,eg,ev,re,r,rp)
+return eg:IsExists(cid.lpfilter,1,nil,lg)
+end
+function cid.lptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return true end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1000)
+    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,1000)
+end
+function cid.lpop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Recover(tp,1000,REASON_EFFECT)
+	local lg=c:GetSummonPlayer()==tp
+	if not lg then return end
+	local g=eg:Filter(cid.lpfilter,nil,lg)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+function cid.revfilter(c,tp)
+	return c:GetSummonLocation()==LOCATION_EXTRA and c:GetPreviousControler()==tp
+		and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP)
+		and bit.band(c:GetPreviousRaceOnField(),RACE_AQUA)>0
+end
+function cid.revcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(cid.spcfilter,1,nil,tp) and not eg:IsContains(e:GetHandler())
+end
+function cid.revtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function cid.revop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		c:RegisterEffect(e1,true)
+	end
+end
