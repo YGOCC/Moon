ab266c1bec8e49371419955cd7a4a7ea384d1525
Clara <lyris89@live.com>
2019-09-28 12:53:53 -0400

    Fix "Paracyclis Officer, Butterfly Commando" by Lyris
    Created by Swag

--- expansions/script/c16028.lua
+++ expansions/script/c16028.lua
@@ -16,7 +16,7 @@ function c16028.initial_effect(c)
 	e2:SetCode(EVENT_SUMMON_SUCCESS)
 	e2:SetTarget(c16028.target)
 	e2:SetOperation(c16028.operation)
-	c:RegisterEffect(e3)
+	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
@@ -25,7 +25,7 @@ function c16028.cfilter(c)
 	return c:IsFaceup() and c:IsRace(RACE_INSECT)
 end
 function c16028.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c65899613.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(c16028.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function c16028.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -50,5 +50,17 @@ function c16028.splimit(e,c)
 	return not c:IsSetCard(0x308)
 end
 function c16028.sfilter(c)
-return c:IsSetCard(0x308) and c:IsAbleToHand() and not c:IsSetCode(16028)
+return c:IsSetCard(0x308) and c:IsAbleToHand() and not c:IsCode(16028)
+end
+function c16028.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c16028.sfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c16028.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c16028.sfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
