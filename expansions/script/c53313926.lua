a5c4a136d5fe83c4bdf7dcd46b3703a4824f52e9
Clara <lyris89@live.com>
2019-11-10 15:35:52 -0500

    Fix redtext on lines 132 and 136 of "Mysterious Cluster Dragon"

--- expansions/script/c53313926.lua
+++ expansions/script/c53313926.lua
@@ -129,11 +129,11 @@ function c53313926.atkop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 	end
 end
-function cid.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c53313926.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
 		and aux.PandSSetCon(e:GetHandler(),nil,e:GetHandler():GetLocation(),e:GetHandler():GetLocation())(nil,e,tp,eg,ep,ev,re,r,rp) end
 end
-function cid.penop(e,tp,eg,ep,ev,re,r,rp)
+function c53313926.penop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and aux.PandSSetCon(e:GetHandler(),nil,e:GetHandler():GetLocation(),e:GetHandler():GetLocation())(nil,e,tp,eg,ep,ev,re,r,rp) then
 		aux.PandSSet(c,REASON_EFFECT,TYPE_EFFECT+TYPE_XYZ)(e,tp,eg,ep,ev,re,r,rp)
