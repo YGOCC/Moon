d1b4796260478b97341b0e4573ee65cbe35ed768
Clara <lyris89@live.com>
2019-12-16 06:54:32 -0500

    Update cards
    Nerf "Inlightened Psychic Helmet" text
    Fix "Blitzkrieg Meknight Dragon - Midnight" text
    Add:
    * "Whirlwind Strike" text
    * "Lyris, Blitzkrieg Tactician of YC.ORG"

--- expansions/script/c102400162.lua
+++ expansions/script/c102400162.lua
@@ -1,1 +1,89 @@
-
+--created by LeonDuvall of Discord, coded by Lyris
+local cid,id=GetID()
+function cid.initial_effect(c)
+	aux.EnablePendulumAttribute(c)
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e0:SetCode(EVENT_DESTROYED)
+	e0:SetRange(LOCATION_PZONE)
+	e0:SetCountLimit(1,id)
+	e0:SetProperty(EFFECT_FLAG_DELAY)
+	e0:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e0:SetCondition(cid.spcon)
+	e0:SetCost(cid.cost)
+	e0:SetTarget(cid.sptg)
+	e0:SetOperation(cid.spop)
+	c:RegisterEffect(e0)
+	local e1=e0:Clone()
+	e1:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e1)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,aux.FilterBoolFunction(Card.IsSetCard,0x7c4))
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,id-1)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e2:SetTarget(cid.tg)
+	e2:SetOperation(cid.op)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(cid.tdcon)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_REMOVE)
+	c:RegisterEffect(e4)
+end
+function cid.cfilter(c,tp)
+	return c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_MZONE) and c:GetPreviousControler()==tp and c:IsType(TYPE_MONSTER) and c:IsSetCard(0x7c4)
+end
+function cid.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(cid.cfilter,1,nil,tp)
+end
+function cid.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x7c4))
+	Duel.RegisterEffect(e1,tp)
+end
+function cid.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function cid.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function cid.filter(c)
+	return c:IsSetCard(0x7c4) and c:IsAbleToDeck()
+end
+function cid.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,3,nil) end
+	local g=Duel.GetMatchingGroup(cid.filter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,3,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function cid.op(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,cid.filter,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,3,3,nil)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+		if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp) end
+		if g:FilterCount(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)==3 then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end
+function cid.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	return c:IsPreviousPosition(POS_FACEUP)
+end
