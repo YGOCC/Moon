bc95a0910620550e74ca5ac7acf7136cd8433423
Lyris <Lyris89@live.com>
2019-12-29 15:54:29 -0500

    Fix "False Reality Distortion"
    Could not be activated

--- expansions/script/c195208413.lua
+++ expansions/script/c195208413.lua
@@ -1,6 +1,10 @@
 --created by Seth, coded by Lyris
 local cid,id=GetID()
 function cid.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
