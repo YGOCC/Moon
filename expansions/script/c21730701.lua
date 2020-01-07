61db3b45d255b62e9a5d55fd09130626077b93f8
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2020-01-07 13:11:21 +0200

    Added Aurams new Prank-Kid card

---
+++ expansions/script/c21730701.lua
@@ -0,0 +1,100 @@
+--Prank-Kids Fuck-Fucking-Fucker
+--By Auramram
+local s,id=GetID()
+function s.initial_effect(c)
+	--link procedure
+	c:EnableReviveLimit()
+	aux.AddLinkProcedure(c,s.mat_filter,1,1)
+	--lock
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetOperation(s.lock_op)
+	c:RegisterEffect(e1)
+	--set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.set_cost)
+	e2:SetTarget(s.set_tg)
+	e2:SetOperation(s.set_op)
+	c:RegisterEffect(e2)
+end
+--link procedure
+function s.mat_filter(c)
+	return c:IsLinkSetCard(0x120) and not c:IsType(TYPE_LINK)
+end
+--lock
+function s.lock_op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsSummonType(SUMMON_TYPE_LINK) then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.lock_tg)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	Duel.RegisterEffect(e2,tp)
+	local e3=Effect.CreateEffect(c)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	e3:SetTargetRange(1,0)
+	Duel.RegisterEffect(e3,tp)
+end
+function s.lock_tg(e,c)
+	return not c:IsSetCard(0x120)
+end
+--set
+function s.set_cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReleasable() end
+	Duel.Release(c,REASON_COST)
+end
+function s.set_filter(c,e)
+	return c:IsSetCard(0x120) and c:IsType(TYPE_SPELL+TYPE_TRAP) 
+		and c:IsSSetable()
+end
+function s.set_tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.set_filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.set_filter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectTarget(tp,s.set_filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+end
+function s.set_op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsSSetable() then
+		Duel.SSet(tp,tc)
+		Duel.ConfirmCards(1-tp,tc)
+		local c=e:GetHandler()
+		if tc:IsType(TYPE_QUICKPLAY) then
+			--workaround while EFFECT_QP_ACT_IN_SET_TURN isn't available
+			if EFFECT_QP_ACT_IN_SET_TURN then
+				local e1=Effect.CreateEffect(c)
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
+				e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+				tc:RegisterEffect(e1)
+			else
+				tc:SetStatus(STATUS_SET_TURN,false)
+			end
+		elseif tc:IsType(TYPE_TRAP) then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+		end
+	end
+end
