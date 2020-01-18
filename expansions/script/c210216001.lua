4748ef99bba2abeeeeefe1ec91289e95c8c46bcc
senpaizuri3 <senpaizuri3@gmail.com>
2020-01-18 01:43:37 -0500

    nerf "Nether Spirit Creation"
    "You cannot Special Summon monsters during the turn you activate either of this card's effects, except "Nether Spirit" monsters."

--- expansions/script/c210216001.lua
+++ expansions/script/c210216001.lua
@@ -5,6 +5,7 @@ function c210216001.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,210216001+EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(c210216001.thcost)
 	e1:SetTarget(c210216001.thtg)
 	e1:SetOperation(c210216001.thop)
 	c:RegisterEffect(e1)
@@ -13,10 +14,30 @@ function c210216001.initial_effect(c)
 	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCost(aux.bfgcost)
+	e2:SetCost(c210216001.cost)
 	e2:SetTarget(c210216001.target)
 	e2:SetOperation(c210216001.activate)
+	Duel.AddCustomActivityCounter(210216001,ACTIVITY_SPSUMMON,c210216001.counterfilter)
 	c:RegisterEffect(e2)
+end
+function c210216001.counterfilter(c)
+	return c:IsSetCard(0x216)
+end
+function c210216001.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost()
+		and Duel.GetCustomActivityCount(210216001,tp,ACTIVITY_SPSUMMON)==0 end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c210216001.splimit)
+	Duel.RegisterEffect(e1,tp)
+end
+function c210216001.splimit(e,c)
+	return not c:IsSetCard(0x216)
 end
 function c210216001.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
@@ -31,6 +52,17 @@ end
 function c210216001.filter(c,e,tp)
 	return c:IsType(TYPE_XYZ) and c:IsRankBelow(4)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false) and c:IsSetCard(0x216)
+end
+function c210216001.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(210216001,tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c210216001.splimit)
+	Duel.RegisterEffect(e1,tp)
 end
 function c210216001.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0
