347ea1ff7a2e7f01d4e1f37544ecaf76c87e14d4
YGOCC <chadoook1990@gmail.com>
2020-01-20 22:15:04 +0100

    fixes

--- expansions/script/c16000786.lua
+++ expansions/script/c16000786.lua
@@ -1,51 +1,55 @@
 --Rank-Up-Magic:VINE's Evolution
-function c16000786.initial_effect(c)
+local cid,id=GetID()
+function cid.initial_effect(c)
 --Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c16000786.target)
-	e1:SetOperation(c16000786.activate)
+	e1:SetCondition(cid.condition)
+	e1:SetTarget(cid.target)
+	e1:SetOperation(cid.activate)
 	c:RegisterEffect(e1)
 	--material
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(16000786,0))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCost(c16000786.matcost)
-	e2:SetTarget(c16000786.mattg)
-	e2:SetOperation(c16000786.matop)
+	e2:SetCost(cid.matcost)
+	e2:SetTarget(cid.mattg)
+	e2:SetOperation(cid.matop)
 	c:RegisterEffect(e2)
 end
-
-function c16000786.filter1(c,e,tp)
+function cid.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_MAIN1 or  Duel.GetCurrentPhase()==PHASE_MAIN2
+end
+function cid.filter1(c,e,tp)
 		local rk=c:GetRank()
 	return c:IsFaceup() and c:IsType(TYPE_XYZ) and (c:IsSetCard(0x85a) or c:IsSetCard(0x85b)) 
-		and Duel.IsExistingMatchingCard(c16000786.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,rk+1)
+		and Duel.IsExistingMatchingCard(cid.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,rk+1)
 end
-function c16000786.filter2(c,e,tp,mc,rk)
+function cid.filter2(c,e,tp,mc,rk)
 			if c.rum_limit_code and code~=c.rum_limit_code then return false end
 	return c:GetRank()==rk  and mc:IsCanBeXyzMaterial(c)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,true,true)
 end
-function c16000786.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c16000786.filter1(chkc,e,tp) end
+function cid.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and cid.filter1(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
-		and Duel.IsExistingTarget(c16000786.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(cid.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,c16000786.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	local g=Duel.SelectTarget(tp,cid.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c16000786.activate(e,tp,eg,ep,ev,re,r,rp)
+function cid.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<0 then return end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c16000786.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetRank()+1)
+	local g=Duel.SelectMatchingCard(tp,cid.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetRank()+1)
 	local sc=g:GetFirst()
 	if sc then
 		local mg=tc:GetOverlayGroup()
@@ -62,26 +66,26 @@ function c16000786.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
-function c16000786.matcost(e,tp,eg,ep,ev,re,r,rp,chk)
-			if chk==0 then return Duel.IsExistingMatchingCard(c16000786.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
+function cid.matcost(e,tp,eg,ep,ev,re,r,rp,chk)
+			if chk==0 then return Duel.IsExistingMatchingCard(cid.cfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c16000786.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,cid.cfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 
-function c16000786.matfilter(c)
+function cid.matfilter(c)
 	return c:IsSetCard(0x285a) and c:IsType(TYPE_XYZ)
 end
-function c16000786.cfilter(c)
+function cid.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x285a) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
 end
-function c16000786.mattg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-		if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c16000786.matfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c16000786.matfilter,tp,LOCATION_MZONE,0,1,nil) end
+function cid.mattg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+		if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and cid.matfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(cid.matfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,c16000786.matfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,cid.matfilter,tp,LOCATION_MZONE,0,1,1,nil)
 end
-function c16000786.matop(e,tp,eg,ep,ev,re,r,rp)
+function cid.matop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
