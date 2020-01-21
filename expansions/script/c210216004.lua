b0d98aeb1f496542653cc0bc1548dfec35409b84
senpaizuri3 <senpaizuri3@gmail.com>
2020-01-20 19:57:10 -0500

    Nerf "Nether Spirit Empire"

--- expansions/script/c210216004.lua
+++ expansions/script/c210216004.lua
@@ -5,6 +5,7 @@ function c210216004.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,210216004)
+	e1:SetTarget(c210216004.cost)
 	e1:SetTarget(c210216004.target)
 	e1:SetOperation(c210216004.activate)
 	c:RegisterEffect(e1)
@@ -28,12 +29,28 @@ function c210216004.initial_effect(c)
 	local e4yx=e4y:Clone()
 	e4yx:SetCode(EFFECT_UNRELEASABLE_NONSUM)
 	c:RegisterEffect(e4yx)
+	Duel.AddCustomActivityCounter(210216004,ACTIVITY_SPSUMMON,c210216004.counterfilter)
+end
+function c210216004.counterfilter(c)
+	return c:IsSetCard(0x216)
 end
 function c210216004.etarget(e,c)
 	return c:GetType()==TYPE_TRAP and c:IsSetCard(0x216)
 end
 function c210216004.searchfilter(c)
 	return c:IsSetCard(0x216) and c:IsAbleToHand()
+end
+function c210216004.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetCustomActivityCount(210216004,tp,ACTIVITY_SPSUMMON)==0 end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(c210216004.splimit)
+	Duel.RegisterEffect(e1,tp)
 end
 function c210216004.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c210216004.searchfilter,tp,LOCATION_DECK,0,1,nil) end
