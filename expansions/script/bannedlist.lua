febaf64dad5870a683d0280d00b6990f513a3917
Lyris <Lyris89@live.com>
2020-01-24 07:51:19 -0500

    Banned list auto-retriever test 2

--- expansions/script/bannedlist.lua
+++ expansions/script/bannedlist.lua
@@ -1,9 +1,8 @@
 if not banned_list_table then banned_list_table={} end
-local string=require('string')
+local string,io=require('string'),require('io')
 local f=debug.getinfo(1,'S')['source']
-Debug.Message(f)
-local io=require('io')
 local l=io.open(f:sub(f:find("expansions"),-22).."lflist.conf","r")
+if not l then return end
 local ls=l:read("*a")
 for id in ls:sub(ls:find("!"),ls:find("!",ls:find("!")+1) and ls:find("!",ls:find("!")+1)-1 or -1):gmatch("([0-9]+) 0") do
 	banned_list_table[tonumber(id)]=true
