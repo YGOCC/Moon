b86bb9194c1f1b64861155d8da96ee1b3d613e13
Lyris <Lyris89@live.com>
2020-01-24 07:11:27 -0500

    Fix banned list retriever (Take 6)

--- expansions/script/bannedlist.lua
+++ expansions/script/bannedlist.lua
@@ -1,6 +1,6 @@
 if not banned_list_table then banned_list_table={} end
-local io,string=require('io'),require('string')
-local l=io.open("expansions/lflist.conf")
+local string=require('string')
+local l=require'io'.open("expansions/lflist.conf")
 local ls=l:read("a*")
 for id in ls:sub(ls:find("!"),ls:find("!",ls:find("!")+1) and ls:find("!",ls:find("!")+1)-1 or -1):gmatch("([0-9]+) 0") do
 	banned_list_table[tonumber(id)]=true
