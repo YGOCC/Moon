8b471afb4985b2c430f34a5bfe6e8db7d20369fe
Lyris <Lyris89@live.com>
2020-01-24 08:09:49 -0500

    Banned list auto-retriever test 8

--- expansions/script/bannedlist.lua
+++ expansions/script/bannedlist.lua
@@ -1,15 +1,12 @@
 if not banned_list_table then banned_list_table={} end
--- local string,io=require('string'),require('io')
+local string,io=require('string'),require('io')
 local f=debug.getinfo(1,'S')['source']
-local e1=Effect.GlobalEffect()
-e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-e1:SetCode(EVENT_PHASE_START+PHASE_STANDBY)
-e1:SetOperation(function() Debug.ShowHint(debug.getinfo(1,'S')['source']) end)
-Duel.RegisterEffect(e1,0)
--- local l=io.open(f:sub(f:find("expansions"),-22).."lflist.conf","r")
--- if not l then return end
--- local ls=l:read("*a")
--- for id in ls:sub(ls:find("!"),ls:find("!",ls:find("!")+1) and ls:find("!",ls:find("!")+1)-1 or -1):gmatch("([0-9]+) 0") do
-	-- banned_list_table[tonumber(id)]=true
--- end
--- l:close()
+local fl=f:find("expansions")
+if not fl then return end
+local l=io.open(f:sub(fl,-22).."lflist.conf","r")
+if not l then return end
+local ls=l:read("*a")
+for id in ls:sub(ls:find("!"),ls:find("!",ls:find("!")+1) and ls:find("!",ls:find("!")+1)-1 or -1):gmatch("([0-9]+) 0") do
+	banned_list_table[tonumber(id)]=true
+end
+l:close()
