684a9d29c6ac640b15f6f392214b99216fdebdcf
Lyris <Lyris89@live.com>
2020-01-24 08:01:19 -0500

    Banned list auto-retriever test 6

--- expansions/script/bannedlist.lua
+++ expansions/script/bannedlist.lua
@@ -1,7 +1,11 @@
 if not banned_list_table then banned_list_table={} end
 -- local string,io=require('string'),require('io')
 local f=debug.getinfo(1,'S')['source']
-Debug.ShowHint(f)
+local e1=Effect.GlobalEffect()
+e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+e1:SetCode(EVENT_PHASE_START+PHASE_DRAW)
+e1:SetOperation(function() Debug.ShowHint(f) end)
+Duel.RegisterEffect(e1,0)
 -- local l=io.open(f:sub(f:find("expansions"),-22).."lflist.conf","r")
 -- if not l then return end
 -- local ls=l:read("*a")
