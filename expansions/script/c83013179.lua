49df2bbe3aed9e8f7cb63ab636a29dbafb737fc1
Lyris <Lyris89@live.com>
2020-02-06 05:34:42 -0500

    Update cards from community packs
    Changed category for "Seatector Landwalker" by Boos
    "Naval Gears Deploy!" bugfix by Renmance

--- expansions/script/c83013179.lua
+++ expansions/script/c83013179.lua
@@ -16,7 +16,6 @@ function cod.initial_effect(c)
 	--Equip
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TODECK)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
