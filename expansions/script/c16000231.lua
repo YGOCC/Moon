e58376e76506fdb6ea2a59e0b0bbf6332eea5459
senpaizuri3 <senpaizuri3@gmail.com>
2020-02-09 17:19:43 -0500

    Update c16000231.lua

--- expansions/script/c16000231.lua
+++ expansions/script/c16000231.lua
@@ -61,39 +61,46 @@ function c16000231.splimit(e,c,sump,sumtype,sumpos,targetp)
 	if c:IsSetCard(0x185a) or c:IsSetCard(0xc4) then return false end
 	return bit.band(sumtype,SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
 end
+function c16000231.tribfilter(c)
+	return c:IsSetCard(0x85a) and not c:IsCode(16000231) and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+end
 function c16000231.filter(c,e,tp,m)
 	if not c:IsSetCard(0x185a) or bit.band(c:GetType(),0x81)~=0x81
 		or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
-	local mg=m:Filter(Card.IsCanBeRitualMaterial,c,c)
+	local mg=m:Filter(c16000231.ritfilter,c,c)
 	if c:IsCode(21105106) then return c:ritual_custom_condition(mg) end
 	if c.mat_filter then
 		mg=mg:Filter(c.mat_filter,nil)
 	end
 	return mg:CheckWithSumEqual(Card.GetRitualLevel,c:GetLevel(),1,99,c)
 end
+function c16000231.ritfilter(c,cc)
+	return c:GetRitualLevel(cc)>0
+end
 function c16000231.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
 function c16000231.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
 	if chk==0 then
-		local mg=Duel.GetRitualMaterial(tp)
-		mg:RemoveCard(e:GetHandler())
-		return Duel.IsExistingMatchingCard(c16000231.filter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,nil,e,tp,mg)
+		local mg=Duel.GetMatchingGroup(c16000231.tribfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,c)
+		return Duel.IsExistingMatchingCard(c16000231.filter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,c,e,tp,mg)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_EXTRA)
 end
 function c16000231.operation(e,tp,eg,ep,ev,re,r,rp)
-	local mg=Duel.GetRitualMaterial(tp)
+	local c=e:GetHandler()
+	local mg=Duel.GetMatchingGroup(c16000231.tribfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,c,e,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local tg=Duel.SelectMatchingCard(tp,c16000231.filter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,1,nil,e,tp,mg)
 	local tc=tg:GetFirst()
 	if tc then
-		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
+		local mg=mg:Filter(c16000231.ritfilter,tc,tc)
 		if tc:IsCode(21105106) then
 			tc:ritual_custom_operation(mg)
 			local mat=tc:GetMaterial()
-			Duel.ReleaseRitualMaterial(mat)
+			Duel.SendtoDeck(mat,nil,2,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
 		else
 			if tc.mat_filter then
 				mg=mg:Filter(tc.mat_filter,nil)
@@ -101,7 +108,7 @@ function c16000231.operation(e,tp,eg,ep,ev,re,r,rp)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 			local mat=mg:SelectWithSumEqual(tp,Card.GetRitualLevel,tc:GetLevel(),1,99,tc)
 			tc:SetMaterial(mat)
-			Duel.ReleaseRitualMaterial(mat)
+			Duel.SendtoDeck(mat,nil,2,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
 		end
 		Duel.BreakEffect()
 		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
