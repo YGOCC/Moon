f2ad246a3bbee979d7c3aa33b65b5c9b817e2bfb
Clara <lyris89@live.com>
2020-02-15 17:16:54 -0500

    Update cards by Lyris
    Remove 2nd confirmation of the card Set by "Mystery Keeper Spharia"
    Fix "Premature Decision" (Take 1)

--- expansions/script/c102400156.lua
+++ expansions/script/c102400156.lua
@@ -81,7 +81,6 @@ function cid.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,cid.ssfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SSet(tp,g:GetFirst())
-		Duel.ConfirmCards(1-tp,g)
 	end
 end
 function cid.filter(c)
