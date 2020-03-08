22375aff38a2962ff5dabc66d401f70806fdf2e9
Clara <lyris89@live.com>
2020-03-08 17:41:19 -0400

    Update cards
    Renewal not sending cards to the deck, just being a rota by Moon
    Move sound files to the appropriate folder

--- expansions/script/c104242576.lua
+++ expansions/script/c104242576.lua
@@ -10,11 +10,10 @@ local id,cid=getID()
 function cid.initial_effect(c)
 		--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TOGRAVE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCountLimit(1,id)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
 	e1:SetTarget(cid.target)
 	e1:SetOperation(cid.activate)
 	c:RegisterEffect(e1)
@@ -36,49 +35,17 @@ function cid.cfilter(c)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x666) and (c:IsAbleToHand() or c:IsAbleToGrave())
 end
 --effect 1
-function cid.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and cid.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(cid.filter,tp,LOCATION_GRAVE,0,2,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,cid.filter,tp,LOCATION_GRAVE,0,2,2,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,2,0,0)
+function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function cid.activate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(44335251,2))
-	local g=Duel.SelectMatchingCard(tp,cid.cfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,cid.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
-		local tc=g:GetFirst()
-		local a=tc:IsAbleToHand()
-		local b=tc:IsAbleToGrave()
-if chk==0 then return a or b end
-if a and b then
-    op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
-elseif a then
-    op=0
-elseif b then
-    op=1
-end
-if op==0 then
-			Duel.SendtoHand(tc,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,tc)
-			local g1=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-			local sg=g1:Filter(Card.IsRelateToEffect,nil,e)
-			if sg:GetCount()>0 then
-			Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
-			end
-			
-			end
-if op==1 then
-			Duel.SendtoGrave(tc,REASON_EFFECT)
-			local g1=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-			local sg=g1:Filter(Card.IsRelateToEffect,nil,e)
-			if sg:GetCount()>0 then
-			Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
-			tc:IsAbleToGrave()
-end
-			
-end
-end
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
 
 
