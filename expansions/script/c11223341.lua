a851e58e03fde992420a9a050d0deca4d694bcde
Clara <lyris89@live.com>
2020-03-12 15:31:58 -0400

    Redesign pict+nerf by saiku
    Scripted by : Rawrock

--- expansions/script/c11223341.lua
+++ expansions/script/c11223341.lua
@@ -2,6 +2,7 @@
 --scripted by Rawstone
 local s,id=GetID()
 function s.initial_effect(c)
+		c:SetUniqueOnField(1,0,79266769)
 		--link summon
 		c:EnableReviveLimit()
 		aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK),2,6,s.lcheck)
@@ -37,20 +38,10 @@ function s.initial_effect(c)
 		e4:SetCode(EVENT_FREE_CHAIN)
 		e4:SetRange(LOCATION_MZONE)
 		e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		e4:SetCountLimit(1,EFFECT_COUNT_CODE_SINGLE)
+		e4:SetCountLimit(1)
 		e4:SetTarget(s.seqtg)
 		e4:SetOperation(s.seqop)
 		c:RegisterEffect(e4)
-		-- Discard
-		local e5=e4:Clone()
-		e5:SetDescription(aux.Stringid(id,1))
-		e5:SetCategory(CATEGORY_HANDES)
-		e5:SetType(EFFECT_TYPE_QUICK_O)
-		e5:SetCode(EVENT_FREE_CHAIN)
-		e5:SetRange(LOCATION_MZONE)
-		e5:SetTarget(s.destg)
-		e5:SetOperation(s.desop)
-		c:RegisterEffect(e5)
 end
 	function s.lcheck(g,lc)
 	return g:IsExists(Card.IsSetCard,1,nil,0x3b)
@@ -61,8 +52,6 @@ end
 	function s.seqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_ONFIELD+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_GRAVE,1,nil) end
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
-	Duel.SetTargetPlayer(tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD+LOCATION_GRAVE,LOCATION_ONFIELD+LOCATION_GRAVE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
@@ -73,16 +62,3 @@ end
 		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 	end
 end
-	function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
-	Duel.SetTargetPlayer(tp)
-	Duel.SetOperationInfo(0,CATEGORY_HANDES,0,0,1-tp,1)
-end
-	function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND,nil)
-	if g:GetCount()==0 then return end
-	local sg=g:RandomSelect(1-tp,1)
-	Duel.SendtoGrave(sg,REASON_DISCARD+REASON_EFFECT)
-end 
-	
