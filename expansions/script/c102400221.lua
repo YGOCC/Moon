76da20a7bdbacc2411d140f22eb668604fa376fe
Clara <lyris89@live.com>
2020-03-14 08:21:10 -0400

    Update cards by Lyris
    Add "YC.Org Registration" script
    Update "Accel Burst Dragon" text from commit be1404d576694fe1c844f91591acef6e70263388

---
+++ expansions/script/c102400221.lua
@@ -0,0 +1,64 @@
+--created by LeonDuvall of Discord, coded by Lyris
+--YC.Orgのレジストレーション
+local cid,id=GetID()
+function cid.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id+EFFECT_COUNT_CODE_OATH)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetOperation(cid.activate)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetTarget(cid.target)
+	e2:SetOperation(cid.operation)
+	c:RegisterEffect(e2)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTarget(cid.desreptg)
+	e2:SetValue(function(e,tc) return cid.repfilter(tc,e:GetHandlerPlayer()) end)
+	e2:SetOperation(function(e) Duel.Destroy(e:GetHandler(),REASON_EFFECT+REASON_REPLACE) end)
+	c:RegisterEffect(e2)
+end
+function cid.filter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x96b) and c:IsAbleToHand()
+end
+function cid.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(cid.filter,tp,LOCATION_DECK,0,1,nil)
+	if #g>0 and Duel.SelectEffectYesNo(tp,c,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function cid.spfilter(c,e,tp)
+	return c:IsSetCard(0x96b) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(cid.spfilter,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_HAND)
+end
+function cid.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cid.spfilter),tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP) end
+end
+function cid.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsSetCard(0x96b) and not c:IsType(TYPE_LINK) and c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function cid.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return rp~=tp and eg:IsExists(cid.repfilter,1,nil,tp)
+		and e:GetHandler():IsDestructable(e) and not c:IsStatus(STATUS_DESTROY_CONFIRMED) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
