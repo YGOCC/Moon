c5e4a7626cf80b1b98bdb8ec8bce88a30a14680e
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2020-03-27 14:17:46 +0200

    Saiku's 3 CD cards

---
+++ expansions/script/c11227787.lua
@@ -0,0 +1,41 @@
+--Card owner: saiku
+function c11227787.initial_effect(c)
+--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(c11227787.target)
+	e1:SetOperation(c11227787.activate)
+	c:RegisterEffect(e1)
+end
+function c11227787.filter(c,e,tp)
+	return c:IsSetCard(0x26E7)
+end
+function c11227787.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c11227787.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function c11227787.dfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x26E7)
+end
+function c11227787.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,c11227787.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if g:GetCount()>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+	local dg=Duel.GetMatchingGroup(c11227787.dfilter,tp,LOCATION_MZONE,0,nil)
+	local tc=dg:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DIRECT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		tc=dg:GetNext()
+	end
+end
