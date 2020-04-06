951e32f5ec0c1c70d132575cdaf69e2c882a0a61
YGOCC <chadoook1990@gmail.com>
2020-04-06 15:41:11 +0100

    Updoots

--- expansions/script/c16000446.lua
+++ expansions/script/c16000446.lua
@@ -1,7 +1,8 @@
---Paintress EX Dark Witch Matissa
-function c16000446.initial_effect(c)
+--ペイントレディ・ローベルタ
+local cid,id=GetID()
+function cid.initial_effect(c)
 	 --link summon
-   aux.AddLinkProcedure(c,nil,2,2,c16000446.lcheck)
+   aux.AddLinkProcedure(c,nil,2,2,cid.lcheck)
 	c:EnableReviveLimit()
 	  --duel status
  --   local e1=Effect.CreateEffect(c)
@@ -17,8 +18,8 @@ function c16000446.initial_effect(c)
 	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e3:SetRange(LOCATION_PZONE)
 	e3:SetTargetRange(LOCATION_MZONE,0)
-	e3:SetTarget(c16000446.tgtg)
-	e3:SetValue(c16000446.indval)
+	e3:SetTarget(cid.tgtg)
+	e3:SetValue(cid.indval)
 	c:RegisterEffect(e3)
 				--indes
 	local e4=Effect.CreateEffect(c)
@@ -26,7 +27,7 @@ function c16000446.initial_effect(c)
 	e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetTargetRange(LOCATION_MZONE,0)
-	e4:SetTarget(c16000446.tgtg)
+	e4:SetTarget(cid.tgtg)
 	e4:SetValue(aux.tgoval)
 	c:RegisterEffect(e4) 
 	--race
@@ -40,95 +41,77 @@ function c16000446.initial_effect(c)
 	c:RegisterEffect(e5) 
 	 --triple nsum
 	local e6=Effect.CreateEffect(c)
-	e6:SetDescription(aux.Stringid(16000446,0))
+	e6:SetDescription(aux.Stringid(id,0))
 	e6:SetCategory(CATEGORY_SUMMON)
 	e6:SetType(EFFECT_TYPE_IGNITION)
 	e6:SetCountLimit(1)
 	e6:SetRange(LOCATION_MZONE)
-	e6:SetCost(c16000446.nscost)
-	e6:SetTarget(c16000446.nstg)
-	e6:SetOperation(c16000446.nsop)
+	e6:SetCost(cid.nscost)
+	e6:SetTarget(cid.nstg)
+	e6:SetOperation(cid.nsop)
 	c:RegisterEffect(e6)
 end
 --filters
-function c16000446.cfilter(c)
+function cid.cfilter(c)
 	return c:IsFaceup() and not c:IsType(TYPE_EFFECT) and c:IsAbleToRemoveAsCost()
 end
-function c16000446.sumfilter(c)
+function cid.sumfilter(c)
 	return c:IsType(TYPE_NORMAL+TYPE_DUAL) and c:IsSummonable(true,nil)
 end
-function c16000446.sumfilterchk(c)
+function cid.sumfilterchk(c)
 	return c:IsSummonable(true,nil)
 end
 --
-function c16000446.lcheck(g,lc)
+function cid.lcheck(g,lc)
 	return g:IsExists(Card.IsSetCard,1,nil,0xc50)
 end
-function c16000446.indval(e,re,tp)
+function cid.indval(e,re,tp)
 	return tp~=e:GetHandlerPlayer()
 end
-function c16000446.tgtg(e,c)
+function cid.tgtg(e,c)
 	return c:IsFaceup() and c:IsType(TYPE_NORMAL) or c:IsType(TYPE_DUAL)
 end
 --triple nsum
-function c16000446.nscost(e,tp,eg,ep,ev,re,r,rp,chk)
-  if chk==0 then return Duel.CheckLPCost(tp,1000) and Duel.IsExistingMatchingCard(c16000446.cfilter,tp,LOCATION_EXTRA,0,1,nil) end
+function cid.nscost(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.CheckLPCost(tp,1000) and Duel.IsExistingMatchingCard(cid.cfilter,tp,LOCATION_EXTRA,0,1,nil) end
 	Duel.PayLPCost(tp,1000)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,c16000446.cfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,cid.cfilter,tp,LOCATION_EXTRA,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c16000446.nstg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c16000446.sumfilter,tp,LOCATION_HAND,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
-end
-function c16000446.nsop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_HAND,0,nil,TYPE_NORMAL+TYPE_DUAL)
-	local sg=g:Filter(c16000446.sumfilterchk,nil)
-	sg:KeepAlive()
-	local check=true
-	local maxct=0
-	while check do
-		if maxct>0 then
-			if not Duel.SelectYesNo(tp,aux.Stringid(16000446,1)) then
-				check=false
-				break
-			end
+function cid.nstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local ct=0
+		local ce={Duel.IsPlayerAffectedByEffect(tp,EFFECT_SET_SUMMON_COUNT_LIMIT)}
+		for _,te in ipairs(ce) do
+			ct=math.max(ct,te:GetValue())
 		end
-		local cg=sg:Clone()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
-		local tc=cg:Select(tp,1,1,nil)
-		local tcg=tc:GetFirst()
-		sg:Sub(tc)
-		Duel.Summon(tp,tcg,true,nil)
-		maxct=maxct+1
-		if maxct>=3 then
-			
-			check=false
-			break
-		end
-		for i in aux.Next(cg) do
-			if i:IsLocation(LOCATION_HAND) and not i:IsSummonable(true,nil) then
-				sg:RemoveCard(i)
-			end
-		end
-		if sg:GetCount()<=0 then
-			check=false
-			break
-		end
+		return ct<3
 	end
-	local e1=Effect.CreateEffect(c)
+end
+function cid.nsop(e,tp,eg,ep,ev,re,r,rp)
+ local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SET_SUMMON_COUNT_LIMIT)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetTargetRange(1,0)
-	e1:SetTarget(c16000446.splimit)
+	e1:SetTarget(cid.xxnstg))
+	e1:SetValue(3)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
+
+   local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetTargetRange(1,0)
+	e2:SetTarget(cid.splimit)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
 end
-function c16000446.splimit(e,c)
+function cid.splimit(e,c)
 	return not c:IsSetCard(0xc50) and c:IsLocation(LOCATION_EXTRA)
 end
-
+function cid.xxnstg(e,c)
+	return and c:IsType(TYPE_NORMAL) or c:IsType(TYPE_DUAL)
+end
