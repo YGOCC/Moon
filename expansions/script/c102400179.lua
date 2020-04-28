335bc515d625acb971cd6e3f7bf217dfbb06b635
Clara <lyris89@live.com>
2020-04-28 10:25:57 -0400

    Update cards by Lyris
    Remove "Newtrix" archetype
    Replace "Newtrix Veil" with "Fate's Ritual Art"

--- expansions/script/c102400179.lua
+++ expansions/script/c102400179.lua
@@ -1,71 +1,84 @@
---created & coded by Lyris, art at https://images-na.ssl-images-amazon.com/images/I/615xevWUOuL._UY741_.jpg
---ニュートリックス・ナイトヴェール
+--created & coded by Lyris
+--フェイツ儀式術
 local cid,id=GetID()
 function cid.initial_effect(c)
+	local getRitLevel=Card.GetRitualLevel
+	Card.GetRitualLevel=function(tc,rc)
+		if tc:IsLocation(LOCATION_SZONE) then
+			local lv,t=tc:GetOriginalLevel(),{tc:IsHasEffect(EFFECT_RITUAL_LEVEL)}
+			if #t>0 then lv=t[#t]:GetValue()(t[#t],rc) end
+			return lv
+		else return getRitLevel(tc,rc) end
+	end
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_LIMIT_ZONE)
-	e1:SetCondition(function(e,tp) return cid.zones(e,tp)>0 end)
-	e1:SetValue(cid.zones)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(cid.RitualUltimateTarget)
+	e1:SetOperation(cid.RitualUltimateOperation)
 	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_LINK_SPELL_KOISHI)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetValue(LINK_MARKER_TOP_LEFT+LINK_MARKER_TOP_RIGHT)
-	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
-	e3:SetRange(LOCATION_SZONE)
-	e3:SetTargetRange(LOCATION_MZONE,0)
-	e3:SetTarget(function(e,tc) return c:GetLinkedGroup():IsContains(tc) and tc:IsSetCard(0xd10) end)
-	c:RegisterEffect(e3)
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_IMMUNE_EFFECT)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-	e4:SetValue(function(e,te) return e:GetOwnerPlayer()~=te:GetOwnerPlayer() end)
-	c:RegisterEffect(e4)
-	e3:SetLabelObject(e4)
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e5:SetCode(EVENT_LEAVE_FIELD_P)
-	e5:SetOperation(function(e) local g=e:GetHandler():GetLinkedGroup() g:KeepAlive() e:SetLabelObject(g) end)
-	c:RegisterEffect(e5)
-	local e6=Effect.CreateEffect(c)
-	e6:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e6:SetCode(EVENT_LEAVE_FIELD)
-	e6:SetOperation(cid.disop)
-	e6:SetLabelObject(e5)
-	c:RegisterEffect(e6)
 end
-function cid.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xd10)
+function cid.matfilter(c)
+	return c:GetOriginalLevel()>0
 end
-function cid.zones(e,tp)
-	local c,g,zone,seq=e:GetHandler(),Duel.GetFieldGroup(tp,LOCATION_MZONE,0),0
-	for tc in aux.Next(g) do
-		seq=tc:GetSequence()
-		if seq>0 and seq<=4 and (Duel.CheckLocation(tp,LOCATION_SZONE,seq-1) or Duel.GetFieldCard(tp,LOCATION_SZONE,seq-1)==c) then zone=bit.replace(zone,0x1,seq-1) end
-		if seq<4 and (Duel.CheckLocation(tp,LOCATION_SZONE,seq+1) or Duel.GetFieldCard(tp,LOCATION_SZONE,seq+1)==c) then zone=bit.replace(zone,0x1,seq+1) end
+function cid.RitualUltimateFilter(c,filter,e,tp,m1,m2,level_function,greater_or_equal,gc)
+	local trap=c:IsLocation(LOCATION_SZONE)
+	if (not trap and c:GetType()&0x81~=0x81) or (filter and not filter(c,e,tp)) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,trap,true) then return false end
+	local mg=m1:Filter(Card.IsCanBeRitualMaterial,c,c)
+	if m2 then
+		mg:Merge(m2)
 	end
-	return zone
-end
-function cid.disop(e,tp,eg,ep,ev,re,r,rp)
-	local g,e1,e0,e3=e:GetLabelObject():GetLabelObject():Filter(Card.IsLocation,nil,LOCATION_MZONE),Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_DISABLE)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD-0x10e0000)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_DISABLE_EFFECT)
-	for tc in aux.Next(g) do
-		e0,e3=e1:Clone(),e2:Clone()
-		tc:RegisterEffect(e0)
-		tc:RegisterEffect(e3)
+	if c.mat_filter then
+		mg=mg:Filter(c.mat_filter,c,tp)
+	else
+		mg:RemoveCard(c)
 	end
-	g:DeleteGroup()
+	if gc then
+		if not mg:IsContains(gc) then return false end
+		Duel.SetSelectedCard(gc)
+	end
+	local lv=trap and c:GetOriginalLevel() or level_function(c)
+	aux.GCheckAdditional=aux.RitualCheckAdditional(c,lv,greater_or_equal)
+	local res=mg:CheckSubGroup(aux.RitualCheck,1,lv,tp,c,lv,greater_or_equal)
+	aux.GCheckAdditional=nil
+	return res
+end
+function cid.RitualUltimateTarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local mg=Duel.GetRitualMaterial(tp)
+		local exg=Duel.GetMatchingGroup(cid.matfilter,tp,LOCATION_SZONE,0,nil)
+		return Duel.IsExistingMatchingCard(cid.RitualUltimateFilter,tp,LOCATION_HAND+LOCATION_SZONE,0,1,nil,aux.FilterBoolFunction(Card.IsSetCard,0xf7a),e,tp,mg,exg,Card.GetLevel,"Greater")
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_SZONE)
+end
+function cid.RitualUltimateOperation(e,tp,eg,ep,ev,re,r,rp)
+	local mg=Duel.GetRitualMaterial(tp)
+	local exg=Duel.GetMatchingGroup(cid.matfilter,tp,LOCATION_SZONE,0,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tg=Duel.SelectMatchingCard(tp,cid.RitualUltimateFilter,tp,LOCATION_HAND+LOCATION_SZONE,0,1,1,nil,aux.FilterBoolFunction(Card.IsSetCard,0xf7a),e,tp,mg,exg,Card.GetLevel,"Greater")
+	local tc=tg:GetFirst()
+	if tc then
+		local trap=tc:IsLocation(LOCATION_SZONE)
+		mg=mg:Filter(Card.IsCanBeRitualMaterial,tc,tc)
+		if exg then
+			mg:Merge(exg)
+		end
+		if tc.mat_filter then
+			mg=mg:Filter(tc.mat_filter,tc,tp)
+		else
+			mg:RemoveCard(tc)
+		end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local lv=tc:GetLevel()
+		if trap then lv=tc:GetOriginalLevel() end
+		aux.GCheckAdditional=aux.RitualCheckAdditional(tc,lv,"Greater")
+		local mat=mg:SelectSubGroup(tp,aux.RitualCheck,false,1,lv,tp,tc,lv,"Greater")
+		aux.GCheckAdditional=nil
+		tc:SetMaterial(mat)
+		Duel.ReleaseRitualMaterial(mat)
+		Duel.BreakEffect()
+		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,trap,true,POS_FACEUP)
+		tc:CompleteProcedure()
+	end
 end
