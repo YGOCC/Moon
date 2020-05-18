1f4ed70bc6ac3776c1ee0f530dee78c2192df366
Clara <lyris89@live.com>
2020-05-18 18:37:32 -0400

    Fix "Paintress Gorgia"
    when she places Pendululm Scales, they are still treated as monsters

--- expansions/script/c16000233.lua
+++ expansions/script/c16000233.lua
@@ -128,7 +128,7 @@ function c16000233.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=cg:GetFirst()
 	Duel.Hint(HINT_CARD,0,tc:GetCode())
 	if b1 and (not b2 or not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) or Duel.SelectYesNo(tp,aux.Stringid(16000233,0))) then
-		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+		Duel.MoveToField(tc,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	else
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
