63a31e8205788b11b809e372774d89924c156cd4
TheBoos2569 <32793818+TheBoos2569@users.noreply.github.com>
2020-05-29 20:21:14 +0300

    Updated cards from community packs

--- expansions/script/c70070085.lua
+++ expansions/script/c70070085.lua
@@ -5,6 +5,14 @@ function cid.initial_effect(c)
 	c:EnableReviveLimit()
 	aux.AddOrigBigbangType(c)
 	aux.AddBigbangProc(c,cid.sfilter,1)
+    --cannot be equip spell target
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(cid.efilter)
+    c:RegisterEffect(e1)
 	--Special summon from grave
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
@@ -36,20 +44,12 @@ function cid.initial_effect(c)
 	e4:SetCondition(cid.atkcond(7600))
 	e4:SetValue(cid.immfilter)
 	c:RegisterEffect(e4)
-	--cannot be target
-	local e7=Effect.CreateEffect(c)
-	e7:SetType(EFFECT_TYPE_SINGLE)
-	e7:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e7:SetRange(LOCATION_MZONE)
-	e7:SetValue(cid.efilter)
-	c:RegisterEffect(e7)
-end
-function cid.efilter(e,re,rp)
-	return re:GetHandler():IsType(TYPE_EQUIP) and not re:GetHandler():IsSetCard(0x777)
 end
 function cid.sfilter(c)
 	return c:IsSetCard(0x777) and aux.FilterEqualFunction(Card.GetVibe,1)
+end
+function cid.efilter(e,re,rp)
+    return re:GetHandler():IsType(TYPE_EQUIP) and not re:GetHandler():IsSetCard(0x777)
 end
 function cid.atkcond(val)
 	return function(e)
@@ -93,7 +93,7 @@ function cid.spop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e2)
-		tc=g:GetNext()
+		tc=sg:GetNext()
 	end
 end
 function cid.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
--- expansions/script/c720059.lua
+++ expansions/script/c720059.lua
@@ -30,6 +30,18 @@ function s.initial_effect(c)
 	e3:SetTarget(s.target)
 	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
+	--plc on top
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TODECK)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCountLimit(1,id+200)
+	e4:SetCondition(s.cornd)
+	e4:SetTarget(s.tdtg)
+	e4:SetOperation(s.tdop)
+	c:RegisterEffect(e4)
 end
 	function s.ntcon(e,c,minc)
 	if c==nil then return true end
@@ -73,7 +85,20 @@ end
 		Duel.SendtoDeck(sg,nil,0,REASON_RULE)
 	end
 end
-
+	function s.cornd(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.filtor2,tp,LOCATION_MZONE,0,1,nil) and Duel.IsExistingMatchingCard(s.filtor,tp,LOCATION_SZONE,0,1,nil)
+end
+	function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeck() end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+	function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsExistingMatchingCard(s.filtor2,tp,LOCATION_MZONE,0,1,nil) then return end
+	if not Duel.IsExistingMatchingCard(s.filtor,tp,LOCATION_SZONE,0,1,nil) then return end
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_EFFECT)
+	end
+end
 
 
 
