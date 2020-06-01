c1fd9402e31a429b63ddbe7de206944be5d269d6
Clara <lyris89@live.com>
2020-06-01 09:41:32 -0400

    Fix cards by Lyris
    "Runefall Berserker" script: Was not setting itself in the S/T zones
    Correct texts:
    * "Stonebreaker Dragon"
    * "Blitzkrieg Meknight - Dusk"
    * "Blitzkrieg Meknight - O'Dark"
    * "Blitzkrieg Element - Flame"
    * "Blitzkrieg Dragon - Sun"
    * "Blitzkrieg Dragon - Gold"
    * "Blitzkrieg Dragon - Obsidian"

--- expansions/script/c52200006.lua
+++ expansions/script/c52200006.lua
@@ -62,48 +62,60 @@ function cid.setop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 	local tc=e:GetHandler()
-	if tc then
+	if tc and tc:IsRelateToEffect(e) then
 		local typ=tc:GetOriginalType()
-		if tc:IsSSetable() then
-			tc:SetCardData(CARDDATA_TYPE,TYPE_TRAP)
+		if aux.PandSSetCon(tc,tp) then
+			local e1=Effect.CreateEffect(tc)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_MONSTER_SSET)
+			e1:SetValue(TYPE_TRAP)
+			tc:RegisterEffect(e1,true)
 			Duel.SSet(tp,tc)
-			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE,1)
+			e1:Reset()
+			tc:SetCardData(CARDDATA_TYPE,TYPE_TRAP)
 			--reset trap status
-			local res=Effect.CreateEffect(e:GetHandler())
-			res:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-			res:SetCode(EVENT_ADJUST)
-			res:SetLabel(typ)
-			res:SetLabelObject(tc)
-			res:SetCondition(cid.rescon)
-			res:SetOperation(cid.reset)
-			Duel.RegisterEffect(res,tp)
+			local e2=Effect.CreateEffect(tc)
+			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e2:SetCode(EVENT_ADJUST)
+			e2:SetRange(LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED+LOCATION_HAND+LOCATION_EXTRA+LOCATION_OVERLAY+LOCATION_MZONE)
+			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SET_AVAILABLE)
+			e2:SetLabelObject(e4)
+			e2:SetOperation(function(ef)
+				local c=ef:GetHandler()
+				if c:GetOriginalType()==TYPE_TRAP then
+					c:AddMonsterAttribute(typ)
+					c:SetCardData(CARDDATA_TYPE,typ)
+					if c:IsSummonType(SUMMON_TYPE_LINK) then c:CompleteProcedure() end
+					ef:Reset()
+				end
+			end)
+			tc:RegisterEffect(e2)
+			local e3=e2:Clone()
+			e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+			e3:SetRange(0)
+			e3:SetCode(EVENT_LEAVE_FIELD)
+			tc:RegisterEffect(e3)
 			--Icarus Attack
-			local e3=Effect.CreateEffect(e:GetHandler())
+			local e3=Effect.CreateEffect(tc)
 			e3:SetCategory(CATEGORY_DESTROY)
 			e3:SetType(EFFECT_TYPE_ACTIVATE)
 			e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 			e3:SetCode(EVENT_FREE_CHAIN)
+			e3:SetCondition(function(ef) return ef:GetHandler():GetFlagEffect(id)>0 end)
 			e3:SetCost(cid.actcost)
 			e3:SetTarget(cid.acttg)
 			e3:SetOperation(cid.act)
-			tc:RegisterEffect(e3)
+			tc:RegisterEffect(e3,true)
 		end
 	end
-	Duel.BreakEffect()
+	if not tc:IsLocation(LOCATION_SZONE) then return end
+	tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE,1)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,cid.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
-	local tc1=g:GetFirst()
-	if tc1 then
-		Duel.SpecialSummon(tc1,0,tp,tp,false,false,POS_FACEUP)
+	if #g>0 then
+		Duel.BreakEffect()
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
-end
---reset trap status
-function cid.rescon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():GetFlagEffect(id)<=0
-end
-function cid.reset(e,tp,eg,ep,ev,re,r,rp)
-	e:GetLabelObject():SetCardData(CARDDATA_TYPE,e:GetLabel())
-	e:Reset()
 end
 --Icarus Attack
 function cid.actcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -136,6 +148,7 @@ function cid.acttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,2,2,c)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,2,0,0)
+	e:Reset()
 end
 function cid.act(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
