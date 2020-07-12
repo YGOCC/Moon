8ecb9a733cad2055aacddd32cc58804ce1cc4257
Clara <lyris89@live.com>
2020-07-12 19:30:42 -0400

    Fix "Awakened - Alvis"

--- expansions/script/c112221913.lua
+++ expansions/script/c112221913.lua
@@ -26,7 +26,7 @@ function cid.initial_effect(c)
 	e4:SetCode(EVENT_PHASE+PHASE_END)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
-	e4:SetOperation(cid.rmop)
+	e4:SetOperation(cid.xdop)
 	c:RegisterEffect(e4)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -114,7 +114,7 @@ function cid.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	Duel.SpecialSummon(Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(cid.filter),tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp),0,tp,tp,false,false,POS_FACEUP)
 end
-function cid.rmop(e,tp,eg,ep,ev,re,r,rp)
+function cid.xdop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:GetOverlayCount()>0 then c:RemoveOverlayCard(tp,1,1,REASON_EFFECT) end
 end
